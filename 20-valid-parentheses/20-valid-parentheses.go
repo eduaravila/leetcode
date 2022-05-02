5249362ec6b7bbeb745918f6abb8a0fc38f4753b
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-05-01 21:50:08 -0500

    Time: 4 ms (12.56%), Space: 2.1 MB (43.47%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.go
+++ 20-valid-parentheses/20-valid-parentheses.go
@@ -1,24 +1,19 @@
 
-func isCompatible(opening rune, closing rune)bool{
-    switch(opening){
-        case '(':
-        if closing == ')'{
-            return true
-        }        
-        case '{':
-        if closing == '}'{
-            return true
-        }
-        case '[':
-        if closing == ']'{
-            return true
-        }        
-    }
-    return false
-}
 
 func isOpening(symbol rune)bool{
     return symbol == '(' || symbol == '[' || symbol == '{'
+}
+
+func antonymOf(symbol rune)rune{
+    switch (symbol){
+        case '(':
+            return ')'
+        case '[':
+            return ']'
+        case '{':
+            return '}'
+    }
+   return 0
 }
 
 func isValid(s string) bool {
@@ -30,10 +25,11 @@ func isValid(s string) bool {
     opening_stack := make([]rune,0)
     
     for _, symbol := range s{
-        if isOpening(symbol){
-            opening_stack = append(opening_stack,symbol)
-        }else if len(opening_stack) > 0 && isCompatible(opening_stack[len(opening_stack)-1], symbol){
-            opening_stack = opening_stack[:len(opening_stack)-1]
+        n := len(opening_stack)-1
+        if isOpening(symbol){            
+            opening_stack = append(opening_stack, antonymOf(symbol))
+        }else if n >= 0 && opening_stack[n]  == symbol {
+            opening_stack = opening_stack[:n]
         }else{
             return false;
         }
