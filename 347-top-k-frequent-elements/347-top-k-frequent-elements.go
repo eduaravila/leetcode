c38b3f0f27c6e85a79679805f802086963ddb109
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-05-05 12:58:36 -0500

    Time: 6 ms (99.05%), Space: 5.5 MB (62.40%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.go
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.go
@@ -1,20 +1,57 @@
+import (
+    "container/heap"
+    "fmt"
+)
+
+
+type element struct {
+    num int
+    freq int
+}
+
+type FreqHeap []*element
+
+func (F FreqHeap) Len()int {return len(F)}
+func (F FreqHeap) Less(a,b int)bool {return F[a].freq < F[b].freq }
+func (F FreqHeap) Swap(a,b int) {    
+    F[a], F[b] = F[b], F[a]
+}
+
+func (F *FreqHeap) Push(elem interface{}) {
+    *F = append(*F, elem.(*element))
+}
+
+func (F *FreqHeap) Pop() interface{}{
+    
+    old := *F // value
+    n := len(old)
+    temp := old[n-1]
+    *F = old[:n-1]
+    
+    return temp
+}
+
 func topKFrequent(nums []int, k int) []int {
     counter := make(map[int]int)
     
-    for _,num := range nums {
+    for _, num := range nums {
         counter[num]++
     }
-    groups := make([][]int,len(nums)+1)
+    minHeap := &FreqHeap{}
+    heap.Init(minHeap)
     
-    for num,freq := range counter{
-        groups[freq] = append(groups[freq], num)
+    for num, freq := range counter {
+        heap.Push(minHeap, &element{num:num, freq:freq} )
+        
+        if minHeap.Len() > k {
+            
+            heap.Pop(minHeap)
+        }
+    }
+    res := []int{}
+    for i := 0 ; i < k ; i++{
+        res = append(res, heap.Pop(minHeap).(*element).num)
     }
     
-    res := make([]int,len(nums))
-    
-    for _,group := range groups{
-        res = append(res, group...)
-    }
-    
-    return res[len(res)-k:]
+    return res
 }
