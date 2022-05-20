09cda57e3ae453e0060f91595cffcc3ad21dad17
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-05-20 16:16:14 -0500

    Time: 563 ms (24.05%), Space: 8.8 MB (68.63%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.go
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.go
@@ -1,7 +1,9 @@
-import 
-(
-    "fmt"
-)
+func getMax(a,b int)int{
+    if a > b {
+        return a
+    }
+    return b
+}
 func longestConsecutive(nums []int) int {
     counter := make(map[int]bool)
     
@@ -9,44 +11,17 @@ func longestConsecutive(nums []int) int {
         counter[num]=true
     }
     
-    uf := make(map[int]int)
-    
-    for _, num := range nums{
-        if _,e := counter[num+1];e{
-            uf[num]= num+1        
-        }else{
-            uf[num] = num
-        }        
-    }
-    
-    for num := range uf {
-        group := []int{}
-        current := num
-        parent := uf[current]
-        for current != parent {
-            group= append(group,current)
-            current = uf[current]
-            parent = uf[current]
+    var res int
+    for _,num := range nums {
+        var groupl int = 0
+        if _,e := counter[num-1]; !e{
+            _, e := counter[num+groupl]
+            for e {                
+                groupl++
+                _, e = counter[num+groupl]
+            }
         }
-        group = append(group,current)
-        
-        for _,elem := range group{
-            uf[elem] = current    
-        }
-        
-        group=[]int{}
-    }   
-    
-    groups := make(map[int]int)
-    for _,group := range uf{
-        groups[group]++
+        res = getMax(groupl,res)        
     }
-    mx := 0
-    
-    for _,val := range groups{
-        if val > mx {
-            mx= val
-        }
-    }
-    return mx
+    return res
 }
