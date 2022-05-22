9c3bafd63d7effd25315a3bc5fd6e8b482850ec5
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-05-22 13:19:22 -0500

    Time: 16 ms (36.57%), Space: 5.7 MB (9.71%) - LeetHub

--- 1-two-sum/1-two-sum.go
+++ 1-two-sum/1-two-sum.go
@@ -1,26 +1,16 @@
 func twoSum(nums []int, target int) []int {
-    counter := make(map[int][]int)
+    counter := make(map[int]int)
     
-    for idx, num := range nums {
-        counter[num] = append(counter[num],idx)
+    for idx,num := range nums {
+        counter[num] =idx // we just need to save the latest index
     }
     
-    
-    res := []int{}
-    for num, idxs := range counter { 
+    for idxc, num := range nums{
         remaining := target - num 
-        res = append(res, idxs[0])
-        if _, e :=counter[remaining];e {
-            if remaining == num && len(idxs) > 1 {
-                res = append(res,idxs[1])
-                return res
-            }else if remaining != num {
-                res = append(res,counter[remaining][0])   
-                return res
-            }            
+        
+        if _, e := counter[remaining]; e && idxc != counter[remaining] {
+            return []int{idxc, counter[remaining]}
         }
-        res = []int{}
     }
-    
-    return res
+    return []int{}
 }
