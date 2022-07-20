b62587cf1502cfb394c182b2db89d2bb32767e6d
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-07-19 22:27:55 -0500

    Time: 9 ms (57.82%), Space: 2.8 MB (81.86%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.go
+++ 242-valid-anagram/242-valid-anagram.go
@@ -1,26 +1,22 @@
 func isAnagram(s string, t string) bool {
-    // count symbols on s and store them on a hash table
-    // iterate through t and check if the current symbol exist on the count hash table
-    // if true,
-    // check if you can reduce 
-    // reduce 1 from the hash table
-    // if the counter is already 0 then that means t is not an anagram
-    // check if the counter if empty meaning we have the same symbols on both strings
-    counter := make(map[rune]int)
-    for _,symbol := range s{
-        counter[symbol]++
+    counterA := make(map[rune]int)
+    counterB := make(map[rune]int)
+    
+    for _,val := range s{
+        counterA[val]++
+    }
+    for _,val := range t{
+        if _,e := counterA[val]; !e{
+            return false
+        }
+        counterB[val]++
     }
     
-    for _,symbol := range t {
-        if _,e := counter[symbol]; e {            
-            counter[symbol]--
-            if counter[symbol] < 1 {
-                delete(counter,symbol)
-            }
-        }else{
+    for letter,counter := range counterA{
+        if counterB[letter] != counter{
             return false
         }
     }
     
-    return len(counter) == 0
+    return true
 }
