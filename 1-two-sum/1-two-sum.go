5a14ac855b471914aa462365fd21c7a0eca2d9ea
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-07-26 00:19:32 -0500

    Time: 11 ms (58.98%), Space: 4.4 MB (33.53%) - LeetHub

--- 1-two-sum/1-two-sum.go
+++ 1-two-sum/1-two-sum.go
@@ -2,8 +2,8 @@ func twoSum(nums []int, target int) []int {
     elements := make(map[int]int)
     for idx,val := range nums {
         rest := target - val
-        if _,e := elements[rest]; e {
-            return []int{idx,elements[rest]}
+        if val,e := elements[rest]; e {
+            return []int{idx,val}
         }
         elements[val] = idx
     }   
