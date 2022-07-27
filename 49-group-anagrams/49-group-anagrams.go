f527792fe7ce6564ade0e75c34dc5a20014924c3
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-07-26 20:48:59 -0500

    Time: 51 ms (31.84%), Space: 8.6 MB (46.72%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.go
+++ 49-group-anagrams/49-group-anagrams.go
@@ -1,28 +1,19 @@
-func getHashKey(str string)string{
-    counter := make([]rune, 26)
-    for _,symbol := range str{
-        counter[symbol -'a']++
-    }
-    return string(counter)
-}
-
+import (
+"strings"
+"sort"
+)
 func groupAnagrams(strs []string) [][]string {
-    // create a hashkey with the words, meaning same symbols in apparence and amount 
-    // results in the same hash key 
-    // use an array of the size of the english alphabet 
-    // the groups hash table will save an array of strings
-    // add the values to the final response
-    
     groups := make(map[string][]string)
-    for _, str := range strs {
-        key := getHashKey(str)
-        groups[key]= append(groups[key],str)
+    for _,str := range strs{
+        sorted := strings.Split(str,"")        
+        sort.Strings(sorted)
+        key := fmt.Sprintf("%v", sorted)
+        
+        groups[key] = append(groups[key],str)
     }
-    
-    res := [][]string{}
-    for _, group := range groups {
-        res = append(res,group)
+    res := make([][]string, 0)
+    for _,group := range groups{
+        res = append(res, group)
     }
-    
-    return res 
+    return res
 }
