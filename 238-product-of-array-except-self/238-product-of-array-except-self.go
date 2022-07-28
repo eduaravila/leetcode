0e511715389212639db0a568a38d3a38eb0b72d4
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-07-27 23:10:47 -0500

    Time: 30 ms (78.62%), Space: 7.5 MB (82.03%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.go
+++ 238-product-of-array-except-self/238-product-of-array-except-self.go
@@ -1,35 +1,18 @@
 func productExceptSelf(nums []int) []int {
-    lr := []int{}
-    rl := []int{}
-    lr = append(lr,nums...)
-    rl = append(rl,nums...)
-    l,r := 0,1
-    for r < len(nums)-1{
-        lr[r] = lr[l] * lr[r]
-        l++
-        r++
+    res := make([]int,0)
+    for range nums{
+        res = append(res,1)
+    }
+    prex := 1 
+    for i:= range res{
+        res[i] = prex
+        prex *= nums[i]
     }
     
-    l,r = len(nums)-1,len(nums)-2
-    for r > 0{
-        rl[r] = rl[l] * rl[r]
-        l--
-        r--
+    posx := 1
+    for i := len(nums)-1 ; i >= 0 ; i--{
+        res[i] *= posx
+        posx *= nums[i]
     }
-    
-    res := make([]int,len(nums))
-    l = 0
-    for l < len(nums){
-        if l-1 < 0 {
-            res[l] = rl[l+1]
-        } else if l+1 >= len(nums){
-            res[l] = lr[l-1]
-        }else{
-            res[l] = lr[l-1] * rl[l+1]        
-        
-        }
-        l++
-    }
-   
     return res 
 }
