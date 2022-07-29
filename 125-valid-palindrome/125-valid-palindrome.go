316e770adddd2f4752f4c8b043a591010feac771
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-07-29 14:33:32 -0500

    Time: 4 ms (71.53%), Space: 2.9 MB (68.03%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.go
+++ 125-valid-palindrome/125-valid-palindrome.go
@@ -1,39 +1,28 @@
-import 
-(
-    "strings"
+import (
+"strings"
 )
-
-func isSpecial(letter byte) bool{
-    return letter < 'a' || letter > 'z'
-}
-
-func isLetter(letter rune) bool{
-    return letter>='a' && letter <='z'
-}
-
-func isNumber(letter rune)bool{ 
-    return letter >= '0' && letter <='9'
-}
-
-func isPalindrome(s string) bool {
+func isPalindrome(input string) bool {
+    s := strings.ToLower(input)
     
-    s = strings.ToLower(s)
-    cleared_s := []rune{}
-    for _, letter := range s {
-        if isNumber(letter) || isLetter(letter){
-            
-            cleared_s = append(cleared_s,letter)
+    l,r := 0, len(s)-1
+    for l < r {
+        if (s[r] <'a' || s[r] >'z') && (s[r] <'0' || s[r] >'9') {
+            r--
+            continue
         }
-    }
-    l := 0
-    r := len(cleared_s)-1
-    fmt.Println(cleared_s)
-    for l < r {                
-        if cleared_s[l] != cleared_s[r]{
+        if (s[l] <'a' || s[l] >'z') && (s[l] <'0' || s[l] >'9') {
+            l++
+            continue
+        }
+        
+        
+        if s[l] != s[r]{
             return false
         }
-        l++
+        
         r--
+        l++
     }
+    
     return true
 }
