a74df091fce50233be5b188b49e806d99b2a971c
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-07-29 15:01:18 -0500

    Time: 29 ms (18.12%), Space: 5.5 MB (12.37%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.go
@@ -1,9 +1,10 @@
 func twoSum(nums []int, target int) []int {        
     l,r := 0,len(nums)-1
     for l < r{
-        if nums[l] + nums[r] > target {
+        currentSum  := nums[l] + nums[r]
+        if currentSum > target {
             r--
-        }else if nums[l] + nums[r] < target {
+        }else if currentSum < target {
             l++
         }else{
             return []int{l+1,r+1}
