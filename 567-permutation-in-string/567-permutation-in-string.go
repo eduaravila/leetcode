ae4fa01b8583082cb89a37baeb7e028bafba60b1
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-02 14:44:26 -0500

    Time: 0 ms (100.00%), Space: 2.5 MB (100.00%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.go
+++ 567-permutation-in-string/567-permutation-in-string.go
@@ -1,29 +1,43 @@
 func checkInclusion(s1 string, s2 string) bool {
-    s1C := make(map[rune]int)
-    
-    for _,val := range s1{
-        s1C[val]++
+    if len(s1) > len(s2){ 
+        return false
     }
+    s1C := make([]int,26)
+    s2C := make([]int,26)
+    for i,val := range s1{
+        s1C[val - 'a']++
+        s2C[rune(s2[i]) - 'a']++
+    }
+    matches := 0
     
-    l,r := 0,0
-    wC := make(map[rune]int)
-    for r < len(s2){
-        wC[rune(s2[r])]++        
-        if r - l+1 == len(s1){
-            valid := true            
-            for k,val := range s1C {                
-                if wC[k] != val {
-                    valid = false
-                    break
-                }
-            }
-            if valid {
-                return valid
-            }
-            wC[rune(s2[l])]--
-            l++
+    for i := 0; i<26 ;i++{
+        if s1C[i] == s2C[i] {
+            matches++
         }
-        r++
     }
-    return false
+    l := 0
+    // start at window end
+    for r:= len(s1) ; r < len(s2) ;r++{
+        if matches == 26{
+            return true
+        }
+        
+        index := rune(s2[r]) - 'a'
+        
+        s2C[index]++
+        if s1C[index] == s2C[index]{
+            matches++
+        }else if s1C[index] +1 == s2C[index]{ // exceed char count 
+            matches--
+        }
+        index = rune(s2[l]) - 'a'
+        s2C[index]--
+        if s1C[index] == s2C[index]{
+            matches++
+        }else if s1C[index] - 1 == s2C[index]{ // removed required values
+            matches--
+        }
+        l++
+    }
+    return matches == 26
 }
