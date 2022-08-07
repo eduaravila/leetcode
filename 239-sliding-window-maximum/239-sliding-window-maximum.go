689b7ad6b60c0142456335d1a89accac5b2623e8
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-06 21:56:26 -0500

    Time: 353 ms (63.07%), Space: 12.3 MB (28.42%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.go
+++ 239-sliding-window-maximum/239-sliding-window-maximum.go
@@ -1,42 +1,26 @@
-import (
-    goheap "container/heap"
-)
 
-type heap []int
-
-func (h *heap) Less(a,b int) bool{return (*h)[a] > (*h)[b] }
-func (h *heap) Len() int { return len(*h)}
-func (h *heap) Swap(a,b int) { (*h)[a],(*h)[b] = (*h)[b],(*h)[a] }
-func (h *heap) Pop() interface{} {
-    temp := (*h)[len(*h)-1]
-    *h = (*h)[:len(*h)-1]
-    return temp
-}
-func (h *heap) Push(val interface{}) {*h = append(*h,val.(int))}
 
 func maxSlidingWindow(nums []int, k int) []int {
-    _h := &heap{}
-    goheap.Init(_h)
-    
-    for i:=0 ; i < k; i++{
-        goheap.Push(_h, nums[i])        
-    }    
     res := []int{}
-    l := 0
-    res = append(res,(*_h)[0])
-    for r := k; r < len(nums); r++{
-        goheap.Push(_h, nums[r])
-        idx := 0 
-        for i,val := range *_h{
-            if val == nums[l]{
-                idx= i
-                break
-            }
+    indexQ := []int{}
+    valQ := []int{}
+    for r,val := range nums{
+        for len(valQ) > 0 && val > valQ[len(valQ)-1]{// keep biggest vals only
+            indexQ = indexQ[:len(indexQ)-1]
+            valQ = valQ[:len(valQ)-1]           
         }
-        goheap.Remove(_h,idx)
-       
-        res = append(res, (*_h)[0])
-        l++
+        indexQ = append(indexQ,r)
+        valQ = append(valQ,val)
+        
+        for r - indexQ[0] + 1 > k { 
+            indexQ = indexQ[1:]
+            valQ = valQ[1:]  
+        }
+        
+        if r + 1 >= k{
+            res = append(res,valQ[0])
+        }
+        
     }
     return res
 }
