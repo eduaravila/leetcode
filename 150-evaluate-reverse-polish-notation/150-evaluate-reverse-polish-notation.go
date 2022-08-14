358459cd829aa5a322d22c747c16a64a856140c6
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-13 19:21:28 -0500

    Time: 12 ms (13.35%), Space: 5.5 MB (5.25%) - LeetHub

---
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.go
@@ -0,0 +1,36 @@
+import ("fmt")
+func performOperation(a,b int, operator string) int{
+    if operator == "+"{
+        return a+b
+    }
+    if operator == "*"{
+        return a*b
+    }
+    if operator == "/"{
+        return a/b
+    }
+    if operator == "-"{
+        return a-b
+    }
+    return 0
+}
+
+func evalRPN(tokens []string) int {
+    nums_stack := []int{}
+    
+    for _, token := range tokens {
+        if token == "+" || token == "-" || token == "*" || token == "/" {
+            a,b :=nums_stack[len(nums_stack)-1],nums_stack[len(nums_stack)-2]                                   
+            nums_stack = nums_stack[:len(nums_stack)-2]
+            fmt.Println(a,b,token)
+            nums_stack  = append(nums_stack,performOperation(b,a,token))            
+        }else{
+            var num int
+            fmt.Sscanf(token,"%d",&num)
+            nums_stack = append(nums_stack,num)
+        }
+
+    }
+    
+    return nums_stack[0]
+}
