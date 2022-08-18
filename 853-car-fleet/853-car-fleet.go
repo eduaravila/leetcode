b38be7ac7dde2aa1e0d6b3e52a36b84cbcafc5a8
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-17 22:18:48 -0500

    Time: 278 ms (61.21%), Space: 16.3 MB (21.55%) - LeetHub

--- 853-car-fleet/853-car-fleet.go
+++ 853-car-fleet/853-car-fleet.go
@@ -1,9 +1,4 @@
-func getMin(a,b int)int {
-    if a < b {
-        return a
-    }
-    return b
-}
+
 func carFleet(target int, position []int, speed []int) int {
     convineTargetSpeed := [][]int{}
     for i,pos := range position {
@@ -14,15 +9,14 @@ func carFleet(target int, position []int, speed []int) int {
     })
     
     
-    stack :=[]float64{}
-    for _, val := range convineTargetSpeed{
-        
-        stack = append(stack, float64(target - val[0]) /float64(val[1]))
-        
-        if len(stack) >= 2 && stack[len(stack)-1] <= stack[len(stack)-2] {
-            stack = stack[:len(stack)-1]
+    
+    stack := []float64{}
+    
+    for _,val := range convineTargetSpeed{
+        stack = append(stack, (float64(target - val[0])) / float64(val[1])) // remaining time to reach target, decimal number
+        if len(stack) > 1 &&  stack[len(stack)-1] <= stack[len(stack)-2]{
+            stack =  stack[:len(stack)-1]
         }
-        
     }
     return len(stack)
 } 
