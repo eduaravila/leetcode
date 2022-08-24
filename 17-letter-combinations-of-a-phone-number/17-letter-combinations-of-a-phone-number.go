3b53005a7fc27faeed29fdb373cadebeb3828bd6
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-24 01:08:15 -0500

    Time: 3 ms (34.40%), Space: 2.2 MB (17.66%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.go
@@ -0,0 +1,32 @@
+import "strings"
+
+var letters = map[string][]string{
+    "2":[]string{"a","b","c"},
+    "3":[]string{"d","e","f"},
+    "4":[]string{"g","h","i"},
+    "5":[]string{"j","k","l"},
+    "6":[]string{"m","n","o"},
+    "7":[]string{"p","q","r","s"},
+    "8":[]string{"t","u","v"},
+    "9":[]string{"w","x","y","z"},
+}
+func letterCombinations(digits string) []string {
+    res := []string{}
+    solution(strings.Split(digits,""),&res,[]string{})
+    return res
+}
+
+func solution(digits []string,res *[]string,current []string)bool{
+    if len(digits) <1 {
+        return true
+    }
+    
+    for _,letter := range letters[digits[0]] {        
+        r := solution(digits[1:],res,append(current,letter))
+        if r {
+            *res= append(*res,strings.Join(append(current,letter),""))
+        }
+    } 
+    
+    return false
+}
