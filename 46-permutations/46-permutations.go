78496fe5904d07117fe6afd86eb3df05dd6ffdd8
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-30 21:42:20 -0500

    Time: 0 ms (100.00%), Space: 2.7 MB (46.61%) - LeetHub

--- 46-permutations/46-permutations.go
+++ 46-permutations/46-permutations.go
@@ -1,18 +1,21 @@
 func permute(nums []int) [][]int {
-    if len(nums) == 1{
-        return [][]int{{nums[0]}}
-    }
     res := [][]int{}
-    for range nums {
-        num := nums[0]        
-        nums = append(nums[:0],nums[1:]...)
-        perms := permute(nums)
-        for i := range perms{  
-            perms[i] = append(perms[i],num)
-        }
-        nums =append(nums,num)
-        res = append(res, perms...)
-    }
+    solution(nums,[]int{},&res)
     return res
 }
 
+func solution(nums []int, permutation []int, res *[][]int)bool{
+    if len(nums) < 1{
+        return true
+    }
+    
+    for i,num := range nums {        
+        numsSlice := append([]int{},nums[0:i]...)
+        numsSlice = append(numsSlice,nums[i+1:]...)
+        if solution(numsSlice, append(permutation,num),res) {
+            *res = append(*res,append(permutation,num))
+        }
+        
+    }
+    return false
+}
