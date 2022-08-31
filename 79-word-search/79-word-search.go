b84e5cf69d340f5f4bc39bd24849b9195b56a2d3
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-31 16:40:11 -0500

    Time: 139 ms (47.43%), Space: 2.1 MB (64.07%) - LeetHub

--- 79-word-search/79-word-search.go
+++ 79-word-search/79-word-search.go
@@ -1,8 +1,14 @@
 func exist(board [][]byte, word string) bool {       
-    
+    visited := [][]bool{}
+    for range board{
+        visited = append(visited,make([]bool,len(board[0])))
+    }
     for x := range board{
-        for y := range board[x]{                        
-            if solution(board,make(map[string]bool),x,y,0,word){
+        for y := range board[x]{
+            if visited[x][y]{
+                continue
+            }
+            if solution(board,visited,x,y,0,word){
                 return true
             }
         }
@@ -10,10 +16,9 @@ func exist(board [][]byte, word string) bool {
     return false
 }
 
-func solution(board [][]byte,visited map[string]bool, x,y,current int, word string)bool{
-    key := fmt.Sprintf("%d,%d",x,y)
+func solution(board [][]byte,visited [][]bool, x,y,current int, word string)bool{
     
-    if x < 0 || x >= len(board) || y < 0 || y >= len(board[0]) || visited[key]  || current >= len(word) {
+    if x < 0 || x >= len(board) || y < 0 || y >= len(board[0]) || visited[x][y]  || current >= len(word) {
         return false
     }        
     if board[x][y] != byte(word[current]) {
@@ -23,10 +28,10 @@ func solution(board [][]byte,visited map[string]bool, x,y,current int, word stri
     if current == len(word)-1 && board[x][y] == byte(word[current]) {
         return true
     }
-    visited[key] = true
+    visited[x][y] = true
     if solution(board,visited,x,y+1,current+1,word) || solution(board,visited,x,y-1,current+1,word) || solution(board,visited,x+1,y,current+1,word) || solution(board,visited,x-1,y,current+1,word){
         return true
     }
-    visited[key] =false
+    visited[x][y] =false
     return false
 }
