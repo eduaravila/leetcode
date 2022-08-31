648ec27d874f9f2f08aa9ddb00027fe84ab4d4d1
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-30 20:12:58 -0500

    Time: 21 ms (7.71%), Space: 7.2 MB (12.21%) - LeetHub

--- 39-combination-sum/39-combination-sum.go
+++ 39-combination-sum/39-combination-sum.go
@@ -5,19 +5,18 @@ func combinationSum(candidates []int, target int) [][]int {
 }
 
 func solution(candidates []int,candidatesI, current int, subset []int, res *[][]int) bool{
-    if current < 0 {
+    if current < 0 || candidatesI >= len(candidates){
         return false
     }
     if current == 0 {
          return true
-    }
-    for i := candidatesI ; i < len(candidates) ; i++ {
-        candidate := candidates[i]
-        subset = append([]int{candidate}, subset...)
-        if solution(candidates, i,current-candidate ,subset, res) {
-            *res  = append(*res, subset)
-        }
-        subset = append([]int{},subset[1:]...)
-    }
+    }    
+    candidate := candidates[candidatesI]
+    subset = append([]int{candidate}, subset...)
+    if solution(candidates, candidatesI,current-candidate ,subset, res) {
+        *res  = append(*res, subset)
+    }    
+    subset = append([]int{},subset[1:]...)
+    solution(candidates, candidatesI+1,current ,subset, res) 
     return false
 }
