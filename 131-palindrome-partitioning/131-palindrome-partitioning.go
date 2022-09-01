d44788a2ec2fb6a72a6f0aaf46a1b2d3b18a0362
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-08-31 20:16:38 -0500

    Time: 700 ms (5.01%), Space: 22.3 MB (49.64%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.go
@@ -0,0 +1,43 @@
+func isPalindrome(input string)bool{
+    if len(input) < 1{
+        return true
+    }
+    
+    l,r := 0, len(input)-1
+    
+    for l < r {
+        if input[l] != input[r]{
+            return false
+        }
+        l++
+        r--
+    }
+    
+    return true 
+}
+
+func partition(s string) [][]string {
+    res := [][]string{}
+    subString := []string{}
+    
+    solution(s,0,&subString,&res)
+    return res
+}
+
+func solution(s string,current int,subString *[]string, res *[][]string ){
+    
+    if current >= len(s){        
+        *res = append(*res,append([]string{},*subString...))
+        return
+    }
+    
+    for i := current ; i < len(s) ; i++ {
+        if isPalindrome(s[current:i+1]){
+            *subString  = append(*subString, s[current:i+1])
+            fmt.Println(*subString)
+            solution(s,i+1,subString,res)
+            *subString = (*subString)[:len(*subString)-1]
+        }
+    }
+    
+}
