4ad36edf81df24d69ad2ce629ff0239c5fc0b03a
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-09-08 03:09:30 -0500

    Time: 6 ms (15.38%), Space: 2.2 MB (31.95%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.go
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.go
@@ -6,33 +6,19 @@
  * }
  */
 func removeNthFromEnd(head *ListNode, n int) *ListNode {
-    stack := []*ListNode{}
-    
-    current := head
-    for current != nil{
-        stack = append(stack,current)
-        current = current.Next
+    temp_head := &ListNode{Val:0,Next:head}
+    r := head
+    for n > 0{
+        r = r.Next
+        n--
     }
     
-    pop_count := 1
-    if len(stack) == 1{
-        return nil
+    l:= temp_head
+    for r != nil{
+        r = r.Next
+        l = l.Next
     }
-    if n == 1 {
-        stack[len(stack)-2].Next = nil   
-        return head
-    }
-    if n == len(stack){
-        head = head.Next
-        return head
-    }
-    current = stack[len(stack)-1]
-    for pop_count < n {
-        pop_count++
-        stack = stack[:len(stack)-1]
-        current = stack[len(stack)-1]
-    }
-    stack[len(stack)-2].Next = current.Next
-    return head
     
+    l.Next = l.Next.Next
+    return temp_head.Next   
 }
