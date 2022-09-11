c1853f700b13044f2f7017dfe71f2f9302efdb75
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-09-10 21:22:54 -0500

    Time: 3 ms (59.21%), Space: 3.6 MB (16.32%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.go
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.go
@@ -8,61 +8,18 @@
  */
 
 func copyRandomList(head *Node) *Node {
-    if head == nil{
-        return head
-    }
+    copy_list := make(map[*Node]*Node)
+    
     current := head
-    head_copy := &Node{}
-    current_copy := head_copy
-    for current != nil {        
-        current_copy.Val= current.Val
+    for current != nil {
+        copy_list[current] = &Node{Val:current.Val}
         current = current.Next
-        if current != nil {
-            current_copy.Next = &Node{}
-            current_copy = current_copy.Next
-        }        
     }
-    
     current = head
-    current_copy = head_copy
-    
-    
-    for current!= nil{
-        
-        current_copy_temp := current_copy.Next
-        
-        current_copy.Next = current.Next
-        current.Next= current_copy        
-        current_copy.Random = current.Random
-        current = current_copy.Next
-        current_copy = current_copy_temp
-        
-    }    
-    
-    current_copy = head_copy
-    for current_copy != nil {
-        
-        if current_copy.Random != nil{
-            current_copy.Random = current_copy.Random.Next
-        }
-        if current_copy.Next == nil{
-            break
-        }
-        current_copy = current_copy.Next.Next
-    }
-    
-    current = head
-    current_copy = head.Next
-    for current != nil && current_copy != nil {
-        
-    
-        current.Next = current_copy.Next
-        if current_copy.Next ==nil{
-            break
-        }
-        current_copy.Next = current.Next.Next
+    for current != nil{
+        copy_list[current].Next= copy_list[current.Next]
+        copy_list[current].Random= copy_list[current.Random]
         current = current.Next
-        current_copy= current_copy.Next
     }
-    return head_copy
+    return copy_list[head]
 }
