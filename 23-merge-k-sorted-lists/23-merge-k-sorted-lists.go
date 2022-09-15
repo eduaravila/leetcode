503c01dcb85318d667d621f80342d0e37bfc6c37
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-09-15 16:45:05 -0500

    Time: 158 ms (26.77%), Space: 5.7 MB (48.99%) - LeetHub

---
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.go
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func mergeList(a,b *ListNode) *ListNode{
+    merged_head := &ListNode{}
+    current := merged_head    
+    for a != nil && b != nil {
+        if a.Val < b.Val {
+            current.Next = a
+            a = a.Next
+        }else{        
+            current.Next = b
+            b = b.Next
+        }    
+        current = current.Next
+    }
+    if a != nil{
+        current.Next = a
+    }
+    if b != nil {
+        current.Next = b
+    }
+    return merged_head.Next
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+    postInf := int(^uint(0)>>1)
+    
+    res := &ListNode{Val: -postInf}
+    
+    for _, list := range lists{
+        fmt.Println(list)
+        res = mergeList(res,list)
+    }
+    
+    return res.Next
+}
