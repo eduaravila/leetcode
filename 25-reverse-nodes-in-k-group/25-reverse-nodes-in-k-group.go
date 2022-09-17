26fa5f52fbe1171aa605bd766ae2da9cd3dd5c6c
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-09-17 18:37:12 -0500

    Time: 0 ms (100.00%), Space: 3.5 MB (88.92%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.go
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.go
@@ -13,21 +13,12 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
     l,r := head,head
     current := 1 
     for r != nil{
-        if current % k == 0{
-            
-            next := r.Next
-            r.Next = nil
-            tempHead := &ListNode{}
-            reverseList(l,nil,tempHead)
-            
-            l.Next = next
-            currentReversed.Next = tempHead.Next
-            currentReversed = l
-            
-            r=l
+        if current % k == 0{            
+            currentReversed.Next = reverseList(l,r)
+            currentReversed = l 
+            r = l
             l = l.Next
-            
-        }        
+        }
         r = r.Next
         current++
     }
@@ -35,12 +26,16 @@ func reverseKGroup(head *ListNode, k int) *ListNode {
     
     return newHead.Next
 }
-
-func reverseList(current, prev , head *ListNode) {
-    if current == nil{        
-        head.Next = prev
-        return 
+// O(k) 
+func reverseList(a,b *ListNode) *ListNode{
+    newHead := b.Next
+    l,r := a, b.Next
+    
+    for l != newHead {
+        temp := l.Next
+        l.Next = r
+        r= l 
+        l = temp                 
     }    
-    reverseList(current.Next,current,head)    
-    current.Next = prev    
+    return r
 }
