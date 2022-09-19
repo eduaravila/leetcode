f6076b52fd75c5cdd6a3e4c5f926f7f8b2ff93e8
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-09-18 22:09:58 -0500

    Time: 34 ms (18.53%), Space: 5.4 MB (42.86%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.go
+++ 42-trapping-rain-water/42-trapping-rain-water.go
@@ -1,24 +1,26 @@
-func getMax( a,b int)int{if a > b {return a}else{return b}}
-func trap(height []int) int {
-    res,l,r := 0,0,len(height)-1
-    lm,rm := height[l],height[r]
-    for l < r {                
-        if height[l] < height[r]{            
-            lm = getMax(lm,height[l])
-            res += lm-height[l]
-            l++
-        }else{            
-            rm = getMax(rm,height[r])
-            res += rm- height[r]            
-            r--
-        }
+func getMax(a,b int)int{
+    if a > b{
+        return a
     }
-    
-    
-    return res
+    return b
 }
 
-// there must be 1 of distance between l and r
-// l and r must be >= than the "middle" elevations
-// if l >= r or r >= l iterate from l to r and add to the result
-// update l and r after that count
+func trap(height []int) int {
+    
+    l,r := 0,len(height)-1
+    lm,rm := height[l],height[r]
+    var res int
+    for l < r {
+        lc,rc := height[l],height[r]
+        if lc < rc {
+            lm = getMax(lc,lm)
+            res += lm - lc
+            l++
+        }else{
+            rm = getMax(rc,rm)
+            res += rm - rc
+            r--
+        }        
+    }
+    return res
+}
