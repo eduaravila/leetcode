e47874954919efb5ee33e25f1d9e981102fbffa2
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-09-22 18:20:16 -0500

    Time: 137 ms (42.17%), Space: 7 MB (87.81%) - LeetHub

--- 55-jump-game/55-jump-game.go
+++ 55-jump-game/55-jump-game.go
@@ -1,43 +1,19 @@
-func getMaxIndex(nums []int, l,r int)int{
-    if nums[l] == 0{
-        return l
+func getMax(a,b int)int{
+    if a > b {
+        return a
     }
-    inf := int(^uint(0)>>1)
-    min := -inf
-    maxI := l+1
-    for i := l+1 ; i <= r ; i++{
-        if i > len(nums) - 1 {
-            break
-        }
-        if nums[i] >= min{
-            min = nums[i]
-            maxI = i
-        }
-    }
-    return maxI
+    return b
 }
 
 func canJump(nums []int) bool {
-    if len(nums) <2 {
-        return true
+    furtest := 0 
+    
+    for i,jumps := range nums{
+        if furtest < i {
+            return false
+        }
+        furtest = getMax(i+jumps,furtest)
     }
     
-    stack := []int{0}
-    for len(stack) > 0{        
-        i := stack[len(stack)-1]
-        current := nums[i]        
-        max := getMaxIndex(nums,i,current+i)
-        if i == len(nums) -1 || max == len(nums)-1 {
-            return true
-        }
-        if nums[max] == 0{
-            nums[i] = 0
-            stack = stack[:len(stack)-1]
-        }else{            
-            stack = append(stack,max)
-        }
-        
-        
-    }
-    return false
+    return true
 }
