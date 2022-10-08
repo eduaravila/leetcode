cfbb2e6f206a4cd779e480e4f4db0ac219261377
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-08 02:10:15 -0500

    Time: 43 ms (39.13%), Space: 6.3 MB (74.78%) - LeetHub

--- 135-candy/135-candy.go
+++ 135-candy/135-candy.go
@@ -6,25 +6,20 @@ func getMax(a,b int)int{
 }
 
 func candy(ratings []int) int {
-    candies := make([]int,len(ratings))
-    for i := range candies {
-        candies[i]=1
-    }
-    
+    candies := make([]int,len(ratings))        
     var i int
-    for i < len(ratings) {
-        
-        
+    for i < len(ratings) {                
         if i > 0 && ratings[i] > ratings[i-1]{
             candies[i] = candies[i-1]+1
-        }        
+        }else{
+            candies[i] = 1
+        }
         i++
     }
     
     i = len(ratings)-1
     
-    for i >= 0 {
-        
+    for i >= 0 {        
         if i > 0 && ratings[i-1] > ratings[i]{
             candies[i-1] = getMax(candies[i]+1, candies[i-1])
         }  
