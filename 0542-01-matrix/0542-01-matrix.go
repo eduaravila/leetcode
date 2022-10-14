88ad4018eea409c69c7e78b94a5d13434918986d
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-14 15:29:45 -0500

    Time: 2881 ms (5.15%), Space: 15.5 MB (5.76%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.go
+++ 0542-01-matrix/0542-01-matrix.go
@@ -1,24 +1,23 @@
 func updateMatrix(mat [][]int) [][]int {
     
     inf := int(^uint(0)>>1)   
-    res := [][]int{}        
+    
     queue := [][]int{}
     for x := range mat{
-        res = append(res,make([]int,len(mat[0])))    
+        
         for y := range mat[x]{
-            if mat[x][y] == 0{
-                res[x][y] = 0
+            if mat[x][y] == 0{ 
                 queue =append(queue,[]int{x,y})
                 continue
             }                       
-            res[x][y] = inf
+            mat[x][y] = inf
         }
     }
-    solution(mat,queue, &res)                
-    return res
+    return solution(mat,queue)
+    
 }
 
-func solution(mat, queue [][]int, res *[][]int){            
+func solution(mat, queue [][]int)[][]int{            
     dirs := [][]int{{-1,0},{1,0},{0,-1},{0,1}}
     for len(queue) >0{
         element := queue[len(queue)-1]
@@ -26,12 +25,12 @@ func solution(mat, queue [][]int, res *[][]int){
         for _,dir := range dirs{                                               
             x_n,y_n := element[0] + dir[0], element[1] + dir[1]
             
-            if x_n < 0 || y_n < 0 || x_n > len(mat) -1 || y_n > len(mat[0]) - 1 || (*res)[x_n][y_n] <= (*res)[element[0]][element[1]]+1 {
+            if x_n < 0 || y_n < 0 || x_n > len(mat) -1 || y_n > len(mat[0]) - 1 || mat[x_n][y_n] <= mat[element[0]][element[1]]+1 {
                 continue
             }            
             queue = append(queue,[]int{x_n,y_n})
-            (*res)[x_n][y_n] = (*res)[element[0]][element[1]]+1
+            mat[x_n][y_n] = mat[element[0]][element[1]]+1
         }
     }
- 
+    return mat
 }
