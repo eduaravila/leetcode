4297e0131e28d0a367fefebb02953c77f14e90f7
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-14 15:45:34 -0500

    Time: 72 ms (81.21%), Space: 7.9 MB (76.67%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.go
+++ 0542-01-matrix/0542-01-matrix.go
@@ -6,11 +6,12 @@ func updateMatrix(mat [][]int) [][]int {
     for x := range mat{
         
         for y := range mat[x]{
-            if mat[x][y] == 0{ 
-                queue =append(queue,[]int{x,y})
+            if mat[x][y] != 0{ 
+                mat[x][y] = inf
                 continue
-            }                       
-            mat[x][y] = inf
+            }   
+            queue =append(queue,[]int{x,y})
+            
         }
     }
     return solution(mat,queue)
@@ -20,17 +21,18 @@ func updateMatrix(mat [][]int) [][]int {
 func solution(mat, queue [][]int)[][]int{            
     dirs := [][]int{{-1,0},{1,0},{0,-1},{0,1}}
     for len(queue) >0{
-        element := queue[len(queue)-1]
-        queue = queue[:len(queue)-1]  
-        for _,dir := range dirs{                                               
-            x_n,y_n := element[0] + dir[0], element[1] + dir[1]
-            
-            if x_n < 0 || y_n < 0 || x_n > len(mat) -1 || y_n > len(mat[0]) - 1 || mat[x_n][y_n] <= mat[element[0]][element[1]]+1 {
-                continue
-            }            
-            queue = append(queue,[]int{x_n,y_n})
-            mat[x_n][y_n] = mat[element[0]][element[1]]+1
+        l := len(queue)
+        for _,element := range queue{        
+            for _,dir := range dirs{                                               
+                x_n,y_n := element[0] + dir[0], element[1] + dir[1]
+                if x_n < 0 || y_n < 0 || x_n > len(mat) -1 || y_n > len(mat[0]) - 1 || mat[x_n][y_n] <= mat[element[0]][element[1]]+1 {
+                    continue
+                }            
+                queue = append(queue,[]int{x_n,y_n})
+                mat[x_n][y_n] = mat[element[0]][element[1]]+1
+            }
         }
+        queue = queue[l:]
     }
     return mat
 }
