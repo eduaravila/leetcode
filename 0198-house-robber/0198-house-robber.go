16a36fae4edfdac2552c590f87520ad405f95246
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-21 17:05:21 -0500

    Time: 0 ms (100.00%), Space: 2 MB (41.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.go
+++ 0198-house-robber/0198-house-robber.go
@@ -7,20 +7,16 @@ func getMax(a,b int)int{
 
 func rob(nums []int) int {
     var max int
-    sums:= append([]int{},nums...)
+    sums:= make([]int,len(nums))
+    
     for i := range nums{
         if i > 1{
             sums[i] = getMax((nums[i]+(sums[i-1] - nums[i-1])), nums[i]+sums[i-2])
+        }else{
+            sums[i] = nums[i]
         }
         max = getMax(max,sums[i])
     }    
     return max
 }
 
-// [2,7,9,3,1]
-// if i > 1 {
-// nums[i] += nums[i-2]
-//}
-// [2,7,11,10,0]
-// 10
-// update max accordingly
