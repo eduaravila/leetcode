4cfd43ec4cfa88c7acbf0c3ca0535c09f458b387
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-22 22:43:45 -0500

    Time: 15 ms (66.72%), Space: 6.5 MB (70.92%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.go
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.go
@@ -7,9 +7,8 @@
  * }
  */
 func kthSmallest(root *TreeNode, k int) int {
-    var value int
-    current := k
-    solution(root,&value, &current)
+    var value int    
+    solution(root,&value, &k)
     return value
 }
 
