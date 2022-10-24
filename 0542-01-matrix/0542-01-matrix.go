3879f8370e6636c2dee6a911f216c3638f11fc52
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-24 13:04:13 -0500

    Time: 138 ms (29.41%), Space: 7.7 MB (82.03%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.go
+++ 0542-01-matrix/0542-01-matrix.go
@@ -1,38 +1,36 @@
 func updateMatrix(mat [][]int) [][]int {
-    
-    inf := int(^uint(0)>>1)   
-    
+    distances := [][]int{}
+    inf := int(^uint(0)>>1)
     queue := [][]int{}
-    for x := range mat{
-        
-        for y := range mat[x]{
-            if mat[x][y] != 0{ 
-                mat[x][y] = inf
-                continue
-            }   
-            queue =append(queue,[]int{x,y})
-            
-        }
-    }
-    return solution(mat,queue)
-    
-}
-
-func solution(mat, queue [][]int)[][]int{            
-    dirs := [][]int{{-1,0},{1,0},{0,-1},{0,1}}
-    for len(queue) >0{
-        l := len(queue)
-        for _,element := range queue{        
-            for _,dir := range dirs{                                               
-                x_n,y_n := element[0] + dir[0], element[1] + dir[1]
-                if x_n < 0 || y_n < 0 || x_n > len(mat) -1 || y_n > len(mat[0]) - 1 || mat[x_n][y_n] <= mat[element[0]][element[1]]+1 {
-                    continue
-                }            
-                queue = append(queue,[]int{x_n,y_n})
-                mat[x_n][y_n] = mat[element[0]][element[1]]+1
+    for y := range mat {
+        distances = append(distances, make([]int,len(mat[0])))
+        for x := range mat[y]{
+            if mat[y][x] != 0 {
+                distances[y][x] = inf
+            }else{
+                queue = append(queue, []int{y,x})
             }
         }
-        queue = queue[l:]
     }
-    return mat
+    
+    
+    coordinates := [][]int{{1,0},{-1,0},{0,1},{0,-1}}
+    for len(queue) > 0 {
+        x,y := queue[0][0],queue[0][1]
+        queue = queue[1:]
+        if mat[x][y] < 0 {
+            continue
+        }
+        for _, coordinate := range coordinates {
+            nx,ny := x + coordinate[0], y + coordinate[1]
+            // outside boundaries or the jump number is bigger than the current
+            if  ny < 0 || nx < 0 || nx > len(mat) -1 || ny > len(mat[0])-1 || distances[x][y] + 1 >= distances[nx][ny]{
+                continue
+            }
+            queue = append(queue,[]int{nx,ny})
+            mat[x][y] = -1
+            distances[nx][ny] = distances[x][y] + 1
+        }
+    }
+    return distances    
 }
