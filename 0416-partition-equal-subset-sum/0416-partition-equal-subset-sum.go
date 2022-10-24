83085d987111c824c274dcc4bc09a949febe5d8d
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-24 18:51:12 -0500

    Time: 430 ms (33.33%), Space: 45.2 MB (15.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.go
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.go
@@ -1,43 +1,38 @@
 func canPartition(nums []int) bool {
-    var sum int 
+    var sum int
     
     for _, num := range nums{
         sum+= num
     }
     if sum % 2 != 0{
         return false
-    }    
-    memo := make(map[int]map[int]bool)
+    }
     
-
-    return getSum(nums, sum/2, 0, memo)
+    return solution(nums,0,sum/2,make(map[int]map[int]bool))
 }
-func getSum(nums []int,sum, current int, memo map[int]map[int]bool )bool{
-    
-    if val, e:= memo[current][sum]; e {        
+
+func solution(nums []int, current,sum int, memo map[int]map[int]bool)bool{
+   
+    if val,e := memo[current][sum]; e {
         return val
     }
-    
-    if sum == 0 {
-        return true
-    }
-    
-    if current > len(nums)-1 || sum < 0  ||( current > len(nums)-1 && sum > 0){
+    if sum < 0 || (sum > 0 && current > len(nums)-1){
         return false
     }
     
-    
-    
-    if getSum(nums, sum,current + 1,memo) || 
-    getSum(nums,sum - nums[current],current + 1,memo){
-        if _,e :=memo[current];!e{
-            memo[current] = make(map[int]bool)
-        }
-        memo[current][sum]= true
+    if sum == 0{
         return true
     }
     
-    if _,e :=memo[current];!e{
+    if solution(nums,current+1,sum-nums[current],memo) || solution(nums,current+1, sum,memo){
+        if _, e := memo[current];!e{
+            memo[current] = make(map[int]bool)
+        }
+        memo[current][sum] = true
+        return true
+    }
+    
+    if _, e := memo[current];!e{
         memo[current] = make(map[int]bool)
     }
     memo[current][sum] = false
