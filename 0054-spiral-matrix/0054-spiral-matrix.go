f691a1a7d86398f948837ffb389238d0ad5aa011
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-25 00:26:23 -0500

    Time: 0 ms (100.00%), Space: 2 MB (74.77%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.go
+++ 0054-spiral-matrix/0054-spiral-matrix.go
@@ -1,25 +1,32 @@
 func spiralOrder(matrix [][]int) []int {
     directions := [][]int{{0,1},{1,0},{0,-1},{-1,0}}
-    var direction int
-    inf := int(^uint(0)>>1)    
-    stack := [][]int{{0,0}}
+    lastDirection := 0
     res := []int{}
-    for len(stack) > 0 {        
-        x,y := stack[len(stack)-1][0],stack[len(stack)-1][1]
-        stack = stack[:len(stack)-1]        
-        res = append(res, matrix[x][y])
-        matrix[x][y] = inf
-        var c_dir int
-        x_new, y_new := x+directions[direction][0], y+directions[direction][1]
-        for c_dir < len(directions) && (x_new < 0 || y_new < 0 || x_new > len(matrix)-1 || y_new > len(matrix[0])-1 || matrix[x_new][y_new] == inf)   {
-            direction = (direction + 1) % len(directions)
-            x_new, y_new = x+directions[direction][0], y+directions[direction][1]
-            c_dir++
-        }        
-        if c_dir > len(directions)-1{
+    
+    queue := [][]int{{0,0}}
+    for len(queue) >0{
+        x,y := queue[len(queue)-1][0],queue[len(queue)-1][1]
+        queue = queue[:len(queue)-1]
+        
+        if matrix[x][y] < -100{
+            continue
+        }
+        
+        res =append(res, matrix[x][y])
+        matrix[x][y] = -101
+        d := 0
+        for d < 4{
+            directionIndex := (lastDirection + d )% 4            
+            direction := directions[directionIndex]
+            xn,yn := direction[0],direction[1]
+            d++
+            if xn+x < 0 || yn+ y < 0 || xn+x > len(matrix)-1 || yn+y > len(matrix[0])-1 || matrix[xn+x][yn+y] < -100{
+                continue
+            }
+            queue = append(queue,[]int{xn+x,yn+y})
+            lastDirection = directionIndex
             break
         }
-        stack = append(stack, []int{x_new,y_new})
     }
     return res
 }
