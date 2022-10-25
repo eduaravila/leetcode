cd4bea4af19c2a22e791116a4454663cc2e1960e
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-24 23:34:48 -0500

    Time: 4 ms (23.23%), Space: 2.5 MB (10.00%) - LeetHub

--- 0078-subsets/0078-subsets.go
+++ 0078-subsets/0078-subsets.go
@@ -1,17 +1,15 @@
 func subsets(nums []int) [][]int {
-    res := [][]int{}
-    solution(nums,[]int{}, &res)
+    res :=  [][]int{}
+    solution(nums,[]int{},&res)
     return res
 }
 
-func solution(nums, current []int, res *[][]int){
-    
+func solution(nums,current []int, res *[][]int){
+    fmt.Println(current,nums)
     *res = append(*res,current)
     for i,num := range nums{
-        current = append(current, num)
-        solution(nums[i+1:],current, res)
+        current = append(current,num)
+        solution(append([]int{},nums[i+1:]...),current,res)
         current = append([]int{},current[:len(current)-1]...)
     }
-    
-    
 }
