48f41e99a46778c538d65d4710303887faced579
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-24 19:00:23 -0500

    Time: 51 ms (7.24%), Space: 9 MB (29.58%) - LeetHub

--- 0155-min-stack/0155-min-stack.go
+++ 0155-min-stack/0155-min-stack.go
@@ -1,40 +1,40 @@
 type MinStack [][]int
 
+
+func Constructor() MinStack {
+    return [][]int{}    
+}
+
 func getMin(a,b int)int{
     if a < b{
         return a
     }
     return b
 }
-func Constructor() MinStack {
-    return [][]int{}
-}
-
-func (this *MinStack) len()int{
-    return len(*this)
-}
 
 func (this *MinStack) Push(val int)  {
-    currentMin := int(^uint(0)>>1)
-    if this.len() > 0{
-        currentMin = this.GetMin()
-    }    
-    *this = append(*this,[]int{val,getMin(currentMin,val)})
+    inf := int(^uint(0)>>1)
+    cMin := inf
+    if len(*this) > 0 {
+        cMin = this.GetMin()
+    }
+    
+    *this = append(*this, []int{val, getMin(cMin,val)})
 }
 
 
 func (this *MinStack) Pop()  {
-    *this = (*this)[:this.len()-1]
+    *this = (*this)[:len(*this) -1]
 }
 
 
 func (this *MinStack) Top() int {
-    return (*this)[this.len()-1][0]
+    return (*this)[len(*this)-1][0]
 }
 
 
 func (this *MinStack) GetMin() int {
-    return (*this)[this.len()-1][1]
+    return (*this)[len(*this)-1][1]
 }
 
 
