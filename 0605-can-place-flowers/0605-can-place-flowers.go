8fcbe9ec900be09bd9fa492f24bdfb1dcd3b9d5b
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-26 22:31:59 -0500

    Time: 40 ms (18.97%), Space: 6.7 MB (12.07%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.go
+++ 0605-can-place-flowers/0605-can-place-flowers.go
@@ -1,12 +1,27 @@
 func canPlaceFlowers(flowerbed []int, n int) bool {
-    var i int 
-    for i < len(flowerbed) {        
-        if flowerbed[i] == 0 && (i < 1 || (flowerbed[i] == 0 && flowerbed[i-1] == 0))  && (i >= len(flowerbed)-1 || (flowerbed[i] == 0 && flowerbed[i+1] == 0)) {
+    
+    if len(flowerbed) < 2 && flowerbed[0] == 0{
+        n--
+    }
+    if len(flowerbed) > 1 && flowerbed[0] == 0 && flowerbed[1] == 0{
+        n--
+        flowerbed[0]=1
+    }
+    if len(flowerbed) > 1 && flowerbed[len(flowerbed)-1] == 0 && flowerbed[len(flowerbed)-2] == 0{
+        n--
+        flowerbed[len(flowerbed)-1]=1
+    }
+    
+    i := 1
+    for i < len(flowerbed)-1 {        
+        if flowerbed[i] == 0 && flowerbed[i-1] == 0 && flowerbed[i+1]==0 {
             n--
-            flowerbed[i] = 1
+            flowerbed[i] = 1 
         }
         i++
     }
+    
+    
     return n < 1
 }
 
