bd659b8d2e687594fc3e6ff96189f807e55086d6
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-26 22:53:04 -0500

    Time: 43 ms (26.70%), Space: 6.2 MB (34.77%) - LeetHub

--- 0169-majority-element/0169-majority-element.go
+++ 0169-majority-element/0169-majority-element.go
@@ -1,6 +1,7 @@
 func majorityElement(nums []int) int {
-    current,count := nums[0],1
-    for _, num := range nums[1:]{
+    inf := int(^uint(0)>>1)
+    current,count := inf, 0
+    for _, num := range nums{
         if num == current{
             count++
         }else if count == 0{
