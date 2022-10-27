1640f3648c68a564ee8b84c10702319727e9a7ea
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-27 18:42:44 -0500

    Time: 102 ms (5.11%), Space: 9.3 MB (5.66%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.go
@@ -0,0 +1,20 @@
+func productExceptSelf(nums []int) []int {
+    left ,right := []int{1},[]int{1}
+    i := 0
+    for i < len(nums)-1{
+        left = append(left, left[i] * nums[i])
+        i++
+    }
+    
+    i = len(nums)-1
+    for i > 0{
+        right = append(right, right[(len(nums)-1)-i] * nums[i])
+        i--
+    }
+    fmt.Println(left,right)
+    res := []int{}
+    for i := range left{
+        res = append(res , left[i]* right[(len(right)-1)-i])
+    }
+    return res
+}
