87574e69902a7dd2521ee8ddcc0f5bdade173aec
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-28 00:12:07 -0500

    Time: 0 ms (100.00%), Space: 2.2 MB (5.13%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.go
+++ 0075-sort-colors/0075-sort-colors.go
@@ -1,7 +1,29 @@
-func sortColors(nums []int)  {
+func sortColors(nums []int)  { 
+    sortq(nums,0,len(nums)-1)
+}
+
+
+func partition(nums []int, low,high int)int{
+    pi := nums[high]
+    i := low-1
+    for low < high{
+        if nums[low] < pi {
+            i++
+            nums[i],nums[low] = nums[low],nums[i]
+        }
+        low++
+    }
+    nums[i+1] ,nums[high] = nums[high] ,nums[i+1]
+    return i +1
+       
+}
+
+func sortq(nums []int, l,r int){
+    if l > r{
+        return 
+    }
     
-    sort.Slice(nums, func(a,b int)bool{
-        return nums[a] < nums[b]
-    })
-    
+    pi :=partition(nums,l,r)
+    sortq(nums,l,pi-1)
+    sortq(nums,pi+1,r)
 }
