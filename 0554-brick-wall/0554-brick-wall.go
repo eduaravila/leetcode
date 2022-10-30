b5dd6c21809b35ac79cd0198409f1c88ac836dfb
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-29 20:39:11 -0500

    Time: 117 ms (5.88%), Space: 7.4 MB (64.71%) - LeetHub

---
+++ 0554-brick-wall/0554-brick-wall.go
@@ -0,0 +1,32 @@
+func getMax(a,b int)int{
+    if a > b{
+        return a
+    }
+    return b
+}
+
+func leastBricks(wall [][]int) int {
+    
+    edges_per_row := make(map[int]int)
+    var total_rows int
+    for _,col := range wall[0]{
+        total_rows+=col
+    }
+    for _, row := range wall{
+        var edge int
+        for _, col := range row[:len(row)-1]{                        
+            edge += col                                    
+            edges_per_row[edge]++
+        }
+    }
+    var max int
+    
+    for _,edges := range edges_per_row{
+        max = getMax(max,edges)
+    }
+    fmt.Println(edges_per_row,total_rows,max)
+    if max < 1{
+        return len(wall)
+    }
+    return len(wall)- max
+}
