cbb16e4581e7f0e29827939cb36e75d04d5a9f57
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-10-31 02:01:10 -0600

    Time: 96 ms (62.85%), Space: 8.3 MB (41.34%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.go
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.go
@@ -1,33 +1,14 @@
 func subarraySum(nums []int, k int) int {
-    var res, sum int
-    
-    
-    for i := range nums{
-        sum = 0
-        for y := i ; y < len(nums); y++{
-            sum += nums[y]
-            if sum == k{
-                res++
-                
-            }
-            
+    prefix := make(map[int]int)
+    var sum, res int
+    prefix[0]++
+    for _,num := range nums{
+        sum += num
+        if val,e := prefix[sum-k];e{
+            res+=val
         }
+        prefix[sum]++
     }
+    
     return res
-}
-
-
-func solution(nums []int, k,current int, res *int){
-    
-    if len(nums) < 1{
-        return
-    }
-    
-    solution(append([]int{},nums[1:]...),k,current+nums[0],res)
-    if current == k{
-        *res++
-    }
-    
-    solution(append([]int{},nums[1:]...),k,nums[0],res)
-
 }
