e3a527a77962805b7282858ce5711d9df9de9efb
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-03 14:46:48 -0600

    Time: 22 ms (73.81%), Space: 10 MB (18.25%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.go
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.go
@@ -1,30 +1,43 @@
-func findRepeatedDnaSequences(s string) []string {    
+func findRepeatedDnaSequences(s string) []string {
     if len(s) < 10{
         return []string{}
     }
-    
-    sequence := []rune{}
-    sequences := make(map[string]int)
-    
-    for i := 0; i < 10 ; i++{
-        sequence = append(sequence,rune(s[i]))
-    }
-    
-    sequences[string(sequence)]++
-    
-    r := 10
-    for r < len(s){        
-        sequence = sequence[1:]
-        sequence = append(sequence,rune(s[r]))
-        r++
-        sequences[string(sequence)]++
-    }
-    
+    var mask int
+    s1 := make(map[int]int)
+    s2 := make(map[int]bool)
     res := []string{}
-    for val,times := range sequences{
-        if times > 1{
-            res = append(res,val)            
+    for i := 0 ; i< 10;i++{
+        mask = mask << 2 | mapToBit(s[i])
+    }
+    s1[mask]++
+    initial := (1 << 20) -1
+    
+    for i:= 10; i< len(s);i++{        
+        mask = (mask << 2) & initial | mapToBit(s[i])  
+        
+        if s2[mask]{
+            continue
         }
+
+        if s1[mask] > 0{
+            s2[mask]= true
+            res = append(res,s[i-9:i+1])
+        }
+        s1[mask]++
     }
     return res
+}
+
+func mapToBit(s byte)int{
+    switch(s){
+        case 'A':
+            return 0
+        case 'C':
+            return 1
+        case 'G':
+            return 2
+        case 'T':
+            return 3
+    }
+    return 0
 }
