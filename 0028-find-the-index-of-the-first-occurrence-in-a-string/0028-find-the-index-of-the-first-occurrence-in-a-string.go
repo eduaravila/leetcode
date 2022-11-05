d30a81a8baee94564072f6ddca6a9d89726991e6
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-04 20:00:51 -0600

    Time: 0 ms (100.00%), Space: 2 MB (70.98%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.go
@@ -1,12 +1,11 @@
 func strStr(haystack string, needle string) int {
-    
-    for l:= 0; ; l++  {
-        for r:=0 ; ; r++{
-            if (r + l) - l == len(needle){return l}
-            if r + l > len(haystack)-1 {return -1}
-            if haystack[r+l] != needle[r]{break}
-        } 
+    for l:=0 ; ; l++{
+        for r:=0 ;; r++{
+            if l+r > len(haystack)-1{return -1}
+            if r > len(needle)-1 {break}
+            if haystack[l+r] != needle[r]{break}
+            if l+r - l +1 == len(needle){return l}
+        }
     }
-    
     return -1
 }
