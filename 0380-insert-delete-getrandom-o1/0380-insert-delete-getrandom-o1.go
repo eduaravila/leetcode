93a8fe99b4b9561deacef188249a9a7b1331f744
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-07 19:22:50 -0600

    Time: 196 ms (84.47%), Space: 54.5 MB (29.61%) - LeetHub

--- 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.go
+++ 0380-insert-delete-getrandom-o1/0380-insert-delete-getrandom-o1.go
@@ -1,51 +1,45 @@
-type RandomizedSet struct{
-    set map[int]int
-    values []int
+type RandomizedSet struct {
+    values map[int]int
+    indexes []int
 }
 
 
 func Constructor() RandomizedSet {
     return RandomizedSet{
-        set: make(map[int]int),
-        values: []int{},
+        values: make(map[int]int),
+        indexes: []int{},
     }
 }
 
 
 func (this *RandomizedSet) Insert(val int) bool {
-    if _,e := this.set[val];e{
+    if _,e := this.values[val]; e{
         return false
     }
-    n := len(this.values)
-    this.set[val]= n
-    this.values = append(this.values,val)
+    this.values[val] = len(this.indexes)
+    this.indexes = append(this.indexes,val)
     return true
 }
 
-
-// use a helper slice
-// remove by moving in place slice[i],slice[len(sluce)-1]
-// and pop the last element
-// make sure to update the new index in the hashtable
-// DO THIS BEFORE REMOVING THE VALEU FROM THE HASHTABLE - **IT COULD BE THE SAME VALUE AND WE DONT WANT TO RE-ADDIT**
 
 func (this *RandomizedSet) Remove(val int) bool {
-    if _,e := this.set[val];!e{
+    if _, e:= this.values[val];!e{
         return false
     }
-    n := this.set[val]
-    this.set[this.values[len(this.values)-1]] = n
+    index := this.values[val]
+    n := len(this.indexes)-1
+    this.indexes[index],this.indexes[n] = this.indexes[n], this.indexes[index]    
+    this.values[this.indexes[index]] = index
     
-    this.values[n], this.values[len(this.values)-1] = this.values[len(this.values)-1], this.values[n]    
-    this.values = this.values[:len(this.values)-1]        
+    delete(this.values,val)
+    this.indexes = this.indexes[:n]
     
-    delete(this.set,val)    
     return true
 }
 
 
-func (this *RandomizedSet) GetRandom() int {        
-    return this.values[rand.Intn(len(this.values))]
+func (this *RandomizedSet) GetRandom() int {
+    return this.indexes[rand.Intn(len(this.indexes))]
 }
 
 
