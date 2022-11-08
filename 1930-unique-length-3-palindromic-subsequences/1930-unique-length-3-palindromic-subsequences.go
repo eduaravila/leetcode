4735f383d674a20d58aedd46d825c43b47c9443e
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-08 12:54:35 -0600

    Time: 854 ms (37.50%), Space: 6.2 MB (68.75%) - LeetHub

--- 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.go
+++ 1930-unique-length-3-palindromic-subsequences/1930-unique-length-3-palindromic-subsequences.go
@@ -1,38 +1,31 @@
 type key struct{
-    mid rune
+    middle rune
     end rune
 }
 
-func isPalindrome(middle rune, preffix map[rune]bool,suffix map[rune]int,res map[key]bool){
+func isPalindrome(pre map[rune]bool, post map[rune]int,c rune, res map[key]bool){
     
-    for i :=0 ; i <26 ;i++{        
-        letter := rune('a' + i)
-        
-        ep := preffix[letter]
-        es := suffix[letter]
-        if ep && es > 0 {
-            res[key{middle,letter}]=true
+    for i := 0; i < 26; i++{
+        char := rune('a'+i)        
+        if pre[char] && post[char] > 0{  
+            res[key{c,char}] = true            
         }
     }
     
 }
 
 func countPalindromicSubsequence(s string) int {
-    preffix := make(map[rune]bool)
-    suffix := make(map[rune]int)
-    
+    pre := make(map[rune]bool)
+    post := make(map[rune]int)
     res := make(map[key]bool)
-    for _,c :=range s{
-        suffix[c]++
+    for _, c := range s{
+        post[c]++
     }
     
-    for _,c := range s{
-        suffix[c]--
-        isPalindrome(c,preffix,suffix,res)
-        preffix[c] = true
-        
+    for _,c := range s{        
+        post[c]--
+        isPalindrome(pre,post,c,res)
+        pre[c] = true                
     }
-    
-    
     return len(res)
 }
