24c537499741a2919d0ef553a0cb65c394d1a0e0
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-08 23:25:05 -0600

    Time: 40 ms (87.33%), Space: 8.1 MB (37.95%) - LeetHub

--- 0015-3sum/0015-3sum.go
+++ 0015-3sum/0015-3sum.go
@@ -1,25 +1,35 @@
 func threeSum(nums []int) [][]int {
     sort.Ints(nums)
     res := [][]int{}
-    for i,num := range nums{
-        if i > 0 && nums[i-1] == nums[i]{
-            continue
-        }
-        l,r := i+1, len(nums)-1
+    solution(nums,[]int{},0,3,0,&res)
+    return res
+}
+
+func solution(nums,current []int, index,k,total int, res *[][]int){
+    if k == 2{
+        l,r := index, len(nums)-1
         for l < r{
-            total := num + nums[l] + nums[r]
-            if total == 0{
-                res = append(res,[]int{num,nums[l],nums[r]})
+            sum := total + nums[l] + nums[r]
+            if sum == 0{
+                *res = append(*res, append([]int{nums[l],nums[r]},current...))
                 r--
                 for l < r && nums[r] == nums[r+1]{
                     r--
                 }
-            }else if total > 0 {
+            }else if sum > 0 {
                 r--
             }else{
                 l++
             }
         }
+        return 
     }
-    return res
+    for i := index ; i < len(nums); i++{
+        if i > index && nums[i-1] == nums[i]{
+            continue
+        }
+        current = append(current,nums[i])
+        solution(nums,current,i+1,k-1,total+ nums[i],res)
+        current = append([]int{},current[:len(current)-1]...)
+    }
 }
