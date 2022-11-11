e6d5eacdf76e609f819d04f37bb626c57b52aff8
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-10 19:12:07 -0600

    Time: 276 ms (75.00%), Space: 9.7 MB (75.00%) - LeetHub

--- 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.go
+++ 1968-array-with-elements-not-equal-to-average-of-neighbors/1968-array-with-elements-not-equal-to-average-of-neighbors.go
@@ -1,21 +1,17 @@
 func rearrangeArray(nums []int) []int {
     sort.Ints(nums)
-    m := int(0+ len(nums)-1)>>1    
+    m := (len(nums)-1) >> 1
     res := make([]int,len(nums))
-    odd:= 0
-    i:=0
-    for i <=m {
-        res[odd] = nums[i]
-        i++
-        odd+=2
-    }
-    pair := 1
-    i= m+1
-    for pair < len(nums){
+    var pair int
+    for i:=0; i <=m;i++{
         res[pair] = nums[i]
-        pair+=2    
-        i++
+        pair+=2
     }
     
+    odd := 1
+    for i := m+1; i < len(nums);i++{
+        res[odd] = nums[i]
+        odd+=2
+    }
     return res
 }
