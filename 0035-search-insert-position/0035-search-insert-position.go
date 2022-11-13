b53e605272554cceef0aa9b758312572a7f64d91
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-13 13:14:35 -0600

    Time: 5 ms (78.33%), Space: 3 MB (8.47%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.go
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -2,18 +2,22 @@ func searchInsert(nums []int, target int) int {
     l,r := 0,len(nums)-1
     
     for l <= r{
-        m := int((l+r) >> 1)
+        m := (int(l+r) >>1)
         
-        if m > len(nums)-1 || nums[m] == target {
+        if nums[m] == target {
             return m
         }
-        
-        if nums[m] > target{
-            r= m-1
+        if nums[m] < target{
+            l = m+1
         }else{
-            l=m+1
-        }
+            r = m-1
+        }        
     }
-    
     return l
 }
+
+
+// 1,3,5,6  -> 7
+// l 0 2 3 4
+// r 3 3 3 3
+// m 1 2 3 
