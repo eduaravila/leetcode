7c02e0116150eff42ff6f144760d00ce79600bf4
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-13 16:41:07 -0600

    Time: 0 ms (100.00%), Space: 2.1 MB (72.92%) - LeetHub

--- 0100-same-tree/0100-same-tree.go
+++ 0100-same-tree/0100-same-tree.go
@@ -7,22 +7,13 @@
  * }
  */
 func isSameTree(p *TreeNode, q *TreeNode) bool {
-    q1 := []*TreeNode{p}
-    q2 := []*TreeNode{q}
-    
-    for len(q1) > 0 && len(q2) > 0 {
-        element1:= q1[0]
-        element2:= q2[0]
-        q1= q1[1:]
-        q2= q2[1:]
-        if element1 == nil && element2 == nil{
-            continue
-        }
-        if element2 == nil ||  element1 == nil || element1.Val != element2.Val {
-            return false
-        }        
-        q1 = append(q1,element1.Right,element1.Left)
-        q2 = append(q2,element2.Right,element2.Left)        
+    if p == nil && q == nil{
+        return true
     }
-    return true
+    
+    if p == nil || q == nil || q.Val != p.Val{
+        return false
+    }
+    
+    return isSameTree(p.Left,q.Left) && isSameTree(p.Right,q.Right)        
 }
