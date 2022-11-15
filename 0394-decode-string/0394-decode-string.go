9cdca4a2bdd4ec01715a415b64cd4a87ee4e632e
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-14 18:27:33 -0600

    Time: 0 ms (100.00%), Space: 2.1 MB (65.82%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.go
@@ -0,0 +1,47 @@
+func decodeString(s string) string {    
+    return solution(s)    
+}
+
+func solution(s string) string{    
+    times := []int{}
+    var i int
+    patterns := []strings.Builder{strings.Builder{}}
+    
+    for i < len(s){            
+        
+        if s[i] >= '0' && s[i] <='9'{
+            index := strings.Index(s[i:],"[")
+            k, _ := strconv.Atoi(s[i:index+i])            
+            times = append(times, k)
+            patterns = append(patterns, strings.Builder{})
+            i += index
+            continue
+        }
+        
+        if s[i] == '['{
+            i++
+            continue   
+        }
+        
+        if s[i] == ']'{
+            k := times[len(times)-1]
+            times = times[:len(times)-1]
+            temp:= patterns[len(patterns)-1]
+            patterns = patterns[:len(patterns)-1]            
+            tempp := patterns[len(patterns)-1].String()
+            patterns[len(patterns)-1].Reset()
+            patterns[len(patterns)-1].WriteString(tempp+strings.Repeat(temp.String(),k))            
+            i++
+            continue   
+        }
+        patterns[len(patterns)-1].WriteString(string(s[i]))
+        i++        
+    }
+    return patterns[0].String()
+}
+
+
+
+// times 3 2 
+// patters a c
+// res 
