732a7a271f3cdb633e39c5023a56d78cd5863002
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-18 16:32:36 -0600

    Time: 31 ms (88.91%), Space: 7.3 MB (41.23%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.go
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.go
@@ -1,41 +1,17 @@
-func mergeSort(input *[]int, l,r int)int{
+func sortedSquares(nums []int) []int {
+    res := make([]int,len(nums))
     
-    pivot :=(*input)[r]
-    i := l-1
-    for l <= r{        
-        if (*input)[l] < pivot {
-            i++
-            (*input)[i],(*input)[l] = (*input)[l], (*input)[i]            
-        }      
-        l++
+    l,r := 0,len(nums)-1
+    i := r
+    for i >= 0{
+        if math.Abs(float64(nums[l])) > math.Abs(float64(nums[r])){
+            res[i] = nums[l] *  nums[l]
+            l++
+        }else{
+            res[i] =  nums[r] *  nums[r]
+            r--
+        }
+        i--        
     }
-    (*input)[r],(*input)[i+1] = (*input)[i+1], (*input)[r]
-    return i+1
+    return res
 }
-
-
-func sort(input *[]int, l,r int){
-    if l >= r{
-        return 
-    }
-    p := mergeSort(input,l,r)
-    sort(input,p+1, r)
-    sort(input,l, p-1)
-}
-
-func sortedSquares(nums []int) []int {    
-    for i, num := range nums{
-        nums[i] = int(math.Pow(float64(num),2))
-    }    
-    
-    sort(&nums,0,len(nums)-1)
-    return nums
-}
-
-// [16,1,0,9,100]
-// l 0
-// r 4
-// p 16
-
-// [9,1,0,16,100]
-
