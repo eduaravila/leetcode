509bc2839f824ecc8aa9a397b7dd4776a45cd74e
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-11-18 15:12:30 -0600

    Time: 56 ms (82.86%), Space: 9.9 MB (100.00%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.go
+++ 0456-132-pattern/0456-132-pattern.go
@@ -1,31 +1,30 @@
 func getMin(a,b int)int{
-    if a < b {
+    if a < b{
         return a
     }
     return b
 }
 
 func find132pattern(nums []int) bool {
-    min := nums[0]
     stack := [][]int{}
+    min := nums[0]
     
-    for _, num := range nums[1:] { 
-        for len(stack) > 0 && num >= stack[len(stack)-1][0]{            
-            stack = stack[:len(stack)-1]
-        }
-        if len(stack) > 0 && num > stack[len(stack)-1][1]{
-            return true
-        }  
-        stack = append(stack,[]int{num,min})
-        min = getMin(min, num)
+    for _, num := range nums[1:]{   
 
-    }    
+        for len(stack) > 0 && num >stack[len(stack)-1][0]{
+            stack = stack[:len(stack)-1]
+        }          
+        if len(stack) > 0 && num < stack[len(stack)-1][0] && num > stack[len(stack)-1][1]{
+            return true
+        }
+        stack = append(stack,[]int{num,min})
+        min = getMin(min,num)
+
+    }
+    
     return false
 }
 
 //[3,5,0,3,4]
-//5,0
 //3,0
-
-// 3,5,4
-// 
+//[5,3],[4,0]
