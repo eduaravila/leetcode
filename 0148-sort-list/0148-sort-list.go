b4f03c38d0f53d955f1a293cd55da5972b309739
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-12-02 19:58:55 -0600

    Time: 212 ms (11.96%), Space: 11.9 MB (5.65%) - LeetHub

---
+++ 0148-sort-list/0148-sort-list.go
@@ -0,0 +1,60 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+func getHalf(head *ListNode) *ListNode{
+    if head == nil{
+        return head
+    }
+    slow,fast := head, head
+    for fast != nil && fast.Next != nil {        
+        fast = fast.Next.Next
+        slow = slow.Next
+    }
+    
+    return slow
+}
+
+func merge(a,b *ListNode)*ListNode{
+    if a == nil {
+        return b
+    }
+    if b == nil {
+        return a
+    }
+    
+    temp := &ListNode{}
+    if a.Val < b.Val {
+        a.Next = merge(a.Next,b)
+        return a
+    }else{
+        b.Next = merge(a,b.Next)
+        return b
+    }
+    
+    return temp
+    
+}
+
+func sortList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil{
+        return head
+    }    
+    mid := getHalf(head)    
+    temp := mid.Next
+    if head.Next == mid {
+        head.Next = nil
+        temp = mid        
+    }else{
+        mid.Next = nil    
+    }
+    l:=sortList(head)
+    r:=sortList(temp)    
+    res := merge(l,r)
+    return res
+}
+
