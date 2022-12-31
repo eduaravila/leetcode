e4a837af4e9016f163e04b56a7ae8ebd485f4579
Eduardo Avila <jesus.amancilla@alumnos.udg.mx>
2022-12-30 19:40:42 -0800

    Time: 19 ms (89.66%), Space: 8.6 MB (96.55%) - LeetHub

--- 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.go
+++ 0894-all-possible-full-binary-trees/0894-all-possible-full-binary-trees.go
@@ -7,6 +7,13 @@
  * }
  */
 func allPossibleFBT(n int) []*TreeNode {
+    return solution(n, make(map[int][]*TreeNode))
+}
+
+func solution(n int, memo map[int][]*TreeNode)[]*TreeNode{
+    if val,e := memo[n]; e{
+        return val
+    }
     if n == 0 {
         return []*TreeNode{}
     }   
@@ -21,8 +28,8 @@ func allPossibleFBT(n int) []*TreeNode {
     for l := 1 ; l < n ; l++{
         r := n - l
         
-        lr,rr := allPossibleFBT(l),allPossibleFBT(r)
-        
+        lr,rr := solution(l,memo),solution(r,memo)
+        memo[l],memo[r] = lr, rr
         for _, noder := range rr{
             for _, nodel := range lr{
                 root := &TreeNode{Val:0,Left:nodel,Right:noder}
