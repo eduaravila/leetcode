ad3f84cc4aef05ef9e66501994a1b267b8b5f160
Eduardo <jesus.amancilla@alumnos.udg.mx>
2023-01-25 23:45:43 -0600

    Time: 13 ms (34.69%), Space: 6.6 MB (14.29%) - LeetHub

--- 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.go
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.go
@@ -13,14 +13,19 @@ func trimBST(root *TreeNode, low int, high int) *TreeNode {
     
     if root.Val < low{
        root = trimBST(root.Right,low,high)
+        if root == nil {
+            return root
+        }
         
-    }else if root.Val > high{
+    } 
+    if root.Val > high{
        root = trimBST(root.Left,low,high)
+        if root == nil{
+            return root
+        }
     }
     
-    if root == nil{
-        return root
-    }
+    
     
     root.Left = trimBST(root.Left,low,high)
     root.Right = trimBST(root.Right,low,high)
