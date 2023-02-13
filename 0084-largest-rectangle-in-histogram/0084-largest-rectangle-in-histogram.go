7762aff878a16e16e2e3b3ec874660dce45aefd9
Eduardo <jesus.amancilla@alumnos.udg.mx>
2023-02-12 19:47:28 -0600

    Time: 92 ms (100.00%), Space: 9.8 MB (46.99%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.go
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.go
@@ -6,30 +6,31 @@ func getMax(a,b int)int{
 }
 
 func largestRectangleArea(heights []int) int {
-    stack := []int{} // monotonic increasing
-    var max , i int
+    var max, i int
+    stack := []int{}
     
-    for i <= len(heights){
+    for i <= len(heights) {                
         var c int
         if i < len(heights){
             c = heights[i]
         }
-        
-        if len(stack) > 0 && c < heights[stack[len(stack)-1]]{
-            element := stack[len(stack)-1]
-            stack = stack[:len(stack)-1]
+        n := len(stack)-1
+        if len(stack) > 0 && c < heights[stack[n]] {
+            e := stack[n]
+            stack = stack[:n]
             if len(stack) < 1{
-                max = getMax(max,heights[element] * i)
+                max = getMax(max, heights[e] * (i))
             }else{
-                max = getMax(max,heights[element] * (i -1 - stack[len(stack)-1]))
-            }
+                max= getMax(max, heights[e] * ((i - 1) - (stack[len(stack)-1])))
+            }            
             i--
-        }else{
+         }else{        
             stack = append(stack,i)
-        }
+         }
         i++
-        
-    }
+    }      
     
-    return max   
-}
+    return max
+}          
+
+
