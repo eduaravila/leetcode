5dd33b1322c338135d3295074512cee4e628ed21
Eduardo <jesus.amancilla@alumnos.udg.mx>
2023-02-13 14:29:02 -0600

    Time: 206 ms (82.76%), Space: 8.5 MB (93.10%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.go
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.go
@@ -1,23 +1,23 @@
-func getMax(a,b int)int{
-    if a > b {
+func getMax(a,b int) int{
+    if a > b{
         return a
     }
     return b
 }
 
-func maxFrequency(nums []int, k int) int {
-    sort.Ints(nums)
-    var res,sum,l,r int
+func maxFrequency(nums []int, k int) int {    
+    var max,l,r,sum int
+    sort.Slice(nums,func(a,b int) bool { return nums[a] < nums[b]})
     
-    for r < len(nums){        
-        sum += nums[r]        
-        window := (r-l+1) * nums[r]        
-        if window > (sum + k){            
+    for r < len(nums) {
+        sum += nums[r]
+        window := ((r-l) +1) * nums[r]
+        if window > (sum+k){
             sum -= nums[l]
             l++
         }
-        res = getMax(res,r-l+1)
+        max = getMax(max, r-l+1)
         r++
     }
-    return res
+    return max
 }
