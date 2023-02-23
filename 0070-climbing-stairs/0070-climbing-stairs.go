8ec65a409f16b6041824c725fe28882b2b49eb2b
Eduardo <jesus.amancilla@alumnos.udg.mx>
2023-02-23 00:23:45 -0600

    Time: 2 ms (6.79%), Space: 2.1 MB (19.90%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.go
+++ 0070-climbing-stairs/0070-climbing-stairs.go
@@ -1,22 +1,20 @@
 func climbStairs(n int) int {
-    return solution(n, make([]int,46))
+    return solution(n, make(map[int]int))
 }
 
-
-func solution(n int, memo []int)int{
-    if memo[n] > 0 {
-        return memo[n]
+func solution(n int, memo map[int]int)int{
+    if val,e := memo[n];e {
+        return val
     }
     
-    if n == 0 {
-        return 1        
+    if n < 0{
+        return 0
     }
-    res := solution(n-1,memo)
+    if n == 0{
+        return 1
+    }
     
-    if n >=2 {
-        res += solution(n-2,memo)        
-        
-    }
-    memo[n] = res
-    return res
+    memo[n-1] = solution(n-1, memo)
+    memo[n-2] = solution(n-2, memo)
+    return memo[n-1] + memo[n-2]
 }
