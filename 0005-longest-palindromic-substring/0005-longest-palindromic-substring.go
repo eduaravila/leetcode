47fb65aa81b7c6be92f22b3384563c1b70d778d7
Eduardo <jesus.amancilla@alumnos.udg.mx>
2023-02-22 18:37:25 -0600

    Time: 3731 ms (5.05%), Space: 84.4 MB (5.18%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.go
@@ -1,23 +1,45 @@
-func longestPalindrome(s string) string {
-    var res string
+func isPalindrome(s string)bool{
+    l,r := 0,len(s)-1
     
-    for i := range s{
-        getBiggestPalindrome(s,i,i, &res)
-        getBiggestPalindrome(s,i,i+1, &res)
+    for l <= r{
+        if s[l]!=s[r]{
+            return false
+        }
+        l++
+        r--
     }
-    return res
+    return true
 }
 
-func getBiggestPalindrome(strn string, s,e int,  res *string){
-    var pal string
-    for s > -1 && e < len(strn) && strn[s] == strn[e]{
-        pal = strn[s:e+1]
-        s--
-        e++
-        
+type key struct{
+    l,r int
+}
+
+func longestPalindrome(s string) string {
+    var max string
+    solution(s,0,len(s),&max, make(map[key]bool))
+    return max
+}
+
+func solution(s string, l, r int,max *string, memo map[key]bool){    
+    _key := key{l,r}
+    if val,e := memo[_key]; e{
+        if val && r-l+1 > len(*max)  {
+            *max = s[l:r]
+        }
+        return
     }
     
-    if len(*res) < len(pal) {
-        *res = pal
-    }    
+    if l > r {
+        return 
+    }
+    
+    if isPalindrome(s[l:r]) && r-l+1 > len(*max){
+        memo[_key] = true
+        *max = s[l:r]
+        return
+    }
+    memo[_key] = false
+    solution(s,l+1,r,max,memo)
+    solution(s,l,r-1,max,memo)
 }
