48243439ea839755e9c4fbecd503929eb2b4e7e4
Eduardo <jesus.amancilla@alumnos.udg.mx>
2023-02-22 18:14:47 -0600

    Time: 4 ms (97.57%), Space: 6.7 MB (39.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.go
+++ 0322-coin-change/0322-coin-change.go
@@ -5,28 +5,20 @@ func getMin(a,b int)int{
     return b
 }
 
-
 func coinChange(coins []int, amount int) int {
-    table := []int{}
-    for i := 0 ; i <= amount;  i++{
-        table = append(table, amount+1)
+    dp := make([]int,amount+1)
+    for i :=1 ; i<= amount;i++{
+        dp[i] = amount+1
     }
     
-    table[0] = 0
-    
-    for _,coin := range coins {        
-        for i := 0; i <= amount; i++{            
-            if i - coin < 0 {
-                continue
-            }
-            table[i] = getMin(table[i], 1+table[i-coin])
+    dp[0] = 0
+    for _,coin := range coins {
+        for i := coin ; i <= amount; i++ {
+            dp[i] = getMin(dp[i], dp[i-coin]+1)
         }
     }
-    
-    if table[amount] == amount+1{
+    if dp[amount] == amount+1{
         return -1
     }
-    return table[amount]
+    return dp[amount]
 }
-
-
