cec92de789e29ebd2d9fcd2a28806aca048c625a
Eduardo <jesus.amancilla@alumnos.udg.mx>
2023-03-04 20:36:23 -0600

    Time: 3 ms (48.00%), Space: 2.2 MB (58.00%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.go
+++ 0097-interleaving-string/0097-interleaving-string.go
@@ -3,17 +3,40 @@ type key struct {
 }
 
 func isInterleave(s1 string, s2 string, s3 string) bool {
-    if len(s1) > len(s3) || len(s2) > len(s3) || len(s1)+len(s2) > len(s3){
+    if len(s1) + len(s2) != len(s3){
         return false
     }
-    return solution(s3, s1, s2, 0, 0, 0, make(map[key]bool))
+    dp := [][]bool{}
+    for i := 0 ; i <= len(s1); i++ {
+        dp = append(dp, make([]bool,len(s2)+1))        
+    }
+    
+    dp[len(s1)][len(s2)] = true
+    
+    for l := len(s1); l >= 0 ; l--{
+        for r := len(s2) ; r >= 0 ; r--{            
+            if l < len(s1) && s1[l] == s3[l+r] && dp[l+1][r]{
+                dp[l][r] = true
+            }
+            if r < len(s2) && s2[r] == s3[l+r] && dp[l][r+1]{
+                dp[l][r] = true
+            }
+        }
+                                        
+    }
+    
+    return dp[0][0]
 }
 
 
 /*
-    a a b c c
-    d b b c a
-
+  a a b c c 
+d  
+b
+b
+c
+a
+            
     a a d b b c b c a c
     
     
@@ -27,7 +50,7 @@ func isInterleave(s1 string, s2 string, s3 string) bool {
     
 */
 
-
+/*
 func solution(s1 string, s2 string, s3 string, i,a,b int, memo map[key]bool) bool{
     k := key{a,b}
     if val,e := memo[k];e{
@@ -64,3 +87,4 @@ func solution(s1 string, s2 string, s3 string, i,a,b int, memo map[key]bool) boo
     
     return false
 }
+*/
