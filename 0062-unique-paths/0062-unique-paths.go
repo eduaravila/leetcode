726716f4d7258cb394a49ec54d10bc09a1815e93
Eduardo <me@eduaravila.com>
2023-03-20 12:46:41 -0600

    Time: 2 ms (31.90%), Space: 2.1 MB (45.70%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.go
+++ 0062-unique-paths/0062-unique-paths.go
@@ -1,20 +1,47 @@
-func swap(a,b *[]int){
-    temp := *a    
-    *a = *b
-    *b = temp
-}
+/*
+
+n, m := len(grid), len(grid[n-1])
+
+if n == 1 and m == 1 
+    result = 1
+
+
+* base case i = n-1 && y == m-1
+
+1,1,1,1,1,1,1
+1,2,3,4,5,6,7
+1,3,6,10,15,21,28
+
+
+res[i][y] = matrix[a][b-1] + matrix[a-1][b]
+
+return res[m][n]
+*/
 
 func uniquePaths(m int, n int) int {
-    col,row := []int{},[]int{}
-    for i := 0; i < n; i++{
-        col = append(col,1)
-        row = append(row,1)
+    if m < 2 || n < 2{
+        return 1
     }
-    for r:= 1 ; r < m ;r++ {
-        for c := 1 ; c < n ; c++ {
-            col[c] = row[c] + col[c-1]
+    
+    res := [][]int{}
+    
+    for i:= 0; i < m; i++{
+        res = append(res, make([]int,n))
+    }
+    
+    res[0][0] = 1
+    
+    for i := 0; i < m ; i++{
+        for y := 0; y < n ; y++{
+            if i > 0 {
+                res[i][y] += res[i-1][y]
+            }
+            if y > 0 {
+                res[i][y] += res[i][y-1]
+            }
+            
         }
-        swap(&col,&row)
     }
-    return row[n-1]
+    
+    return res[m-1][n-1]
 }
