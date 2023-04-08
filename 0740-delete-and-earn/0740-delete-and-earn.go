b58243b11f03e2d37857b9fd48c8d6e8251d727d
Eduardo <me@eduaravila.com>
2023-04-08 15:46:17 -0600

    Time: 11 ms (50.37%), Space: 6.2 MB (43.70%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.go
+++ 0740-delete-and-earn/0740-delete-and-earn.go
@@ -1,50 +1,39 @@
-
-func getMax(a,b int) int{
-    if a > b{
+func getMax(a,b int )int{
+    if a > b {
         return a
     }
     return b
 }
 
+
 func deleteAndEarn(nums []int) int {
+    set := []int{}
     counter := make(map[int]int)
     
-    set := []int{}
-    
-    for _, num := range nums {
-        if _, e := counter[num]; !e {            
+    for _, num := range nums{
+        if _,e := counter[num]; !e{
             set = append(set, num)
         }
-        counter[num]++        
+        counter[num]++
     }
-    sort.Ints(set)
-    dp := make([]int, len(set))
-    dp[0] = counter[set[0]] * set[0]
     
-    i := 1
-    for i < len(set) {
-        num := set[i]
-        current := counter[num] * num
+    
+    sort.Ints(set)
+    var a,b int 
+    
+    for i, num := range set {
+        current := num * counter[num]
         
-        if i > 1 && set[i-1] == num-1 {
-            dp[i] = getMax(dp[i-1], dp[i-2] +current)
-        }else if i >1 {
-            dp[i]= getMax(current + dp[i-1], current + dp[i-2])
-        }else if set[i-1] < num-1{
-            dp[i]= getMax(current, current + dp[i-1])
+        if i > 0 && set[i-1] == num-1{
+            temp := b
+            b = getMax(b , current + a)
+            a = temp
         }else{
-            dp[i]= getMax(current, dp[i-1])
+            temp := b
+            b += current
+            a = temp 
         }
-        i++
-        
     }
-    fmt.Println(dp,set) 
-    return dp[len(set)-1]
+    
+    return b
 }
-
-/*
-[8,10,4,9,1,3,5,9,4,10]
-
-[1,3,4,5,8,9,10]
-
-*/
