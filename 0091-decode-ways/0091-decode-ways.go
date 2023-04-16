1492b48990dd0252f27f5765aeb2869e8025a13a
Eduardo <me@eduaravila.com>
2023-04-15 19:33:37 -0600

    Time: 0 ms (100.00%), Space: 2.3 MB (14.45%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.go
+++ 0091-decode-ways/0091-decode-ways.go
@@ -1,78 +1,68 @@
-func numDecodings(s string) int {
-    return solution(s,0,make(map[int]int))
+
+/*
+if it starts at 0 it returns 0 
+  0 -> 0
+  01 -> 0
+  
+only 1 and 2 accept two digits 
+
+
+only 1 accept digits from 0 to 9
+
+2 from 0 to 6
+
+
+if current has two digits return 1
+
+12 
+i
+
+12
+ i
+ 
+after all the other conditiosn have passed 
+ 
+if i > len(s){
+return 1
 }
 
-func solution(s string, l int,memo map[int]int)int{
-        
-    if l > len(s)-1 {
-         return 1 // last value we assume we found a valid way
-    }
-    if val,e := memo[l]; e{
+*/
+
+func numDecodings(s string) int {
+
+    
+    return solution(s, 0, make(map[int]int))
+}
+
+func solution(s string, l int, memo map[int]int) int{
+    
+    if val,e := memo[l];e{
         return val
     }
-   
-    i, _ := strconv.Atoi(s[l:l+1])
-    if i == 0{
+    
+    if l > len(s)-1{        
+        return 1
+    }
+ 
+    lnum, _ :=strconv.Atoi(string(s[l]))
+    
+    if lnum == 0 {
         return 0
     }
     
     var res int 
-    if i == 1 && l < len(s)-1 {
-        res += solution(s,l+2, memo)
+    if lnum == 1 && l < len(s)-1 {
+        res += solution(s,l+2,memo)        
     }
-    
     
     if l < len(s)-1 {
-        nextI, _ := strconv.Atoi(s[l+1:l+2]) 
-        if i == 2 && nextI <= 6{
-            res += solution(s,l+2, memo)
-        }
+        rnum, _ := strconv.Atoi(string(s[l+1:l+2]))
+        if lnum == 2 &&  rnum <=6 {        
+            res +=solution(s,l+2, memo)
+        }            
     }
-   
-    res += solution(s, l+1, memo)
-    memo[l] = res
-    return res
-}
-
-/*
-AAJF
-
-
-    11106
     
- var l, r -> save the current index for s 
- 
- if r > len(s) -1 {
-     return 1 // last value we assume we found a valid way
- }
- 
- check if current is in range 
- 
- if s[l:r] < 'a' || s[l:r]  > 'z' {
- // 0
- // 35
-    return 0    // this is invalid
- }
- 
-return solution(s, r,r+1)  + solution(s, r, r+2)
-
-"226"
-
-
-2 , 26,
-2, 2, 6 
-
-
-22, 6
-
-
-
-
-202
-
-20,2
-
-
-
-
-*/
+    res += solution(s,l+1, memo)
+    memo[l] = res
+    return res   
+}
