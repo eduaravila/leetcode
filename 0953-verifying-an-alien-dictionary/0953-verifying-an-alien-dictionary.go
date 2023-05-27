e5050a72a3e023c2faff96d762c61a48f89b39ab
Eduardo <me@eduaravila.com>
2023-05-26 21:58:20 -0600

    Time: 4 ms (28.79%), Space: 2.8 MB (16.67%) - LeetHub

--- 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.go
+++ 0953-verifying-an-alien-dictionary/0953-verifying-an-alien-dictionary.go
@@ -1,27 +1,52 @@
 func isAlienSorted(words []string, order string) bool {
-    dict := make(map[rune]int)
+    dict_indexes := make(map[rune]int)
     
-    for i,c := range order{
-        dict[c] = i
+    for i, c := range order{
+        dict_indexes[c]=i+1
     }
+    dict_indexes[' '] = 0
     
-    
-    for x:= 0; x <len(words)-1;x++{
-        w1,w2 := words[x],words[x+1]
-        for j := 0 ; j < len(w1) ; j++{
-            // second word is smaller
-            if j ==len(w2){
+    var i int
+    for i < len(words)-1{
+
+        a, b := words[i], words[i+1]
+        
+        var aindex,bindex int
+        for aindex < len(a) || bindex < len(b){
+            var aletter, bletter rune
+            if aindex < len(a){
+                aletter = rune(a[aindex])
+                aindex++
+                
+            }
+            
+            if bindex < len(b){
+                bletter = rune(b[bindex])
+                bindex++
+            }           
+            
+            if dict_indexes[aletter] < dict_indexes[bletter]{
+                break
+            }
+            if dict_indexes[aletter] > dict_indexes[bletter]{
                 return false
             }
             
-            if w1[j] != w2[j]{
-                if dict[rune(w1[j])] > dict[rune(w2[j])]{
-                    return false
-                }
-                break
-            }
             
         }
+        i++
     }
+    
     return true
 }
+
+/*
+
+   compare two words 
+    
+    for l <lenb(a) || r < len(b)
+        
+        if dict[a[l]] > dict[b[r]]        
+            return false
+    
+*/
