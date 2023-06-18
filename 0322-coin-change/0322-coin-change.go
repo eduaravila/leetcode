3d6eb68a93ca03ee208663a0897561fc05c88822
Eduardo <me@eduaravila.com>
2023-06-17 21:56:31 -0600

    Time: 24 ms (38.94%), Space: 7.5 MB (27.24%) - LeetHub

--- 0322-coin-change/0322-coin-change.go
+++ 0322-coin-change/0322-coin-change.go
@@ -1,24 +1,65 @@
 func getMin(a,b int)int{
-    if a < b{
+    if a < b {
         return a
     }
     return b
 }
 
 func coinChange(coins []int, amount int) int {
-    dp := make([]int,amount+1)
-    for i :=1 ; i<= amount;i++{
-        dp[i] = amount+1
+    dp := []int{}
+    
+    inf := int(^uint(0)>>1)
+    
+    for i := 0 ; i <= amount ; i++{
+        dp = append(dp, inf)
     }
     
     dp[0] = 0
-    for _,coin := range coins {
-        for i := coin ; i <= amount; i++ {
-            dp[i] = getMin(dp[i], dp[i-coin]+1)
+    
+    for _, coin := range coins{
+        var i int
+        next := i + coin
+        for next < amount+1 {
+            if dp[i] == inf{
+                i++
+                next = i + coin
+                continue
+            }
+            dp[next] = getMin(dp[next], dp[i]+1) 
+            i++
+            next = i + coin
         }
     }
-    if dp[amount] == amount+1{
+    
+ 
+    if dp[amount] == inf{
         return -1
     }
-    return dp[amount]
+
+    return dp[amount]       
 }
+
+
+/*
+
+Input: coins = [1,2,5], amount = 11, answ = 3
+
+
+[0,1,1,2,2,3,3,4,4,5,5,6] 
+       
+       
+       
+dp := []int, len(coints) +1
+
+next = current + coin
+
+if next < len(coins){
+    next = min(dp[next], current + 1)
+}
+
+if min == inf{
+    return -1
+}
+
+return dp[n]
+*/
