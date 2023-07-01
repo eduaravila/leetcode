0838b38c9b9e631cd9333fc4a0e82d433390af8b
Eduardo <me@eduaravila.com>
2023-07-01 00:14:56 -0500

    Time: 2879 ms (6.25%), Space: 125.9 MB (6.25%) - LeetHub

---
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.go
@@ -0,0 +1,135 @@
+func getMax(a,b int)int{
+    if a> b{
+        return a
+    }
+    return b
+}
+
+func findMaxFormsDp(pairs [][]int,current, availableZ, availableO int, memo map[string]int )int{
+    key := fmt.Sprintf("%i,%i,%i",current,availableZ,availableO)
+    if val, e := memo[key]; e{
+        return val
+    }
+    if current >= len(pairs) || availableZ < 0 || availableO < 0 {
+        return 0
+    }
+    
+    if pairs[current][0] > availableZ  || pairs[current][1] > availableO {
+        memo[key] = findMaxFormsDp(pairs,current+1,availableZ,availableO, memo)
+        return memo[key]
+    }
+    
+    include := 1 + findMaxFormsDp(pairs, current+1, availableZ - pairs[current][0], availableO - pairs[current][1], memo)
+    exclude := findMaxFormsDp(pairs, current+1, availableZ, availableO, memo)
+    memo[key] =  getMax(include, exclude)
+    return memo[key]
+}
+
+func findMaxForm(strs []string, m int, n int) int {
+    sets := [][]int{}
+   for _,str := range strs {
+        ones,zeros := 0,0
+        for _, c := range str{
+            if c == '1'{
+                ones++
+            }else{
+                zeros++
+            }
+        }
+        sets = append(sets, []int{zeros,ones})
+    }
+    return findMaxFormsDp(sets,0,m,n, make(map[string]int))
+}
+
+/*
+m = 0's 
+n = 1's
+
+
+base case 
+
+m = 0
+n = 0 
+
+res = 0
+
+
+["10","0001","111001","1","0"]
+
+set = (zero, one)
+
+[[1,1],[3,1],[2,3],[0,1],[1,0]]
+
+[0,1],[1,0],[1,1],[3,1],[2,3]
+
+[0,1],[1,1],[2,2],[5,3],[7,6]
+
+
+["10","0","1"]
+
+[1,0],[0,1],[1,1]
+
+[1,0],[1,1],[2,2]
+
+
+count the number on sets 
+
+sort the sets
+
+
+prefix sum
+compare a pair with n and m until it satisfies the condition and return the index
+
+dp[i][j] = max(dp[i][j],1+dp[i-currZeros][j-currOnes]);
+
+
+[0,1],[1,0],[1,1],[3,1],[2,3]
+
+i = 5
+x = 3,2
+
+[0,0,0]
+[0,0,0]
+[0,0,0]
+[0,0,0]
+[0,0,1]
+
+*/
+
+/*
+ sets := [][]int{}
+    
+    for _,str := range strs {
+        ones,zeros := 0,0
+        for _, c := range str{
+            if c == '1'{
+                ones++
+            }else{
+                zeros++
+            }
+        }
+        sets = append(sets, []int{zeros,ones})
+    }
+    
+    sort.Slice(sets, func(a,b int)bool{
+        return sets[a][0] < sets[b][0] && sets[a][1] < sets[b][1]
+    })
+    
+
+    fmt.Println(sets)
+    var max int
+    for i := range sets{   
+        var prevZ,prevO int
+        for currentSet := i ; currentSet< len(sets);currentSet++{
+            set := sets[currentSet]
+            prevZ += set[0]
+            prevO += set[1]
+            
+            if prevZ > m || prevO > n{
+                break
+            }
+            max = getMax(max, (currentSet-i)+1)        
+        }
+    }    
+    return max
+*/
